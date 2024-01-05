2c292cb8120536d283a0413f695ba0362f2f634c
tonytc13579 <tony@trustless.computer>
2024-01-05 09:56:36 +0700

    change text

--- src/modules/home/Section_2/config.ts
+++ src/modules/home/Section_2/config.ts
@@ -12,7 +12,7 @@ export type SlideItemType = {
   }[];
 };
 
-export const DataList: SlideItemType[] = [
+export const DataListOriginal: SlideItemType[] = [
   {
     key: 'Alpha Chain',
     title: 'Alpha Chain',
@@ -97,97 +97,10 @@ export const DataList: SlideItemType[] = [
       {
         icon: '/icons/storage_ic.svg',
         desc: 'Data Availability',
-        desc1: 'Polygon',
-      },
-    ],
-  },
-  {
-    key: 'Alpha Chain 100',
-    title: 'Alpha Chain',
-    srcImg: '/images/ai_chain_img.png',
-    href: 'https://alpha.wtf',
-    isViewProject: true,
-    isComminSoon: false,
-    childrentList: [
-      {
-        icon: '/icons/layer_ic.svg',
-        desc: 'Use Case',
-        desc1: 'SocialFi',
-      },
-      {
-        icon: '/icons/infrastructure_ic.svg',
-        desc: 'Rollups',
-        desc1: 'Optimistic, Sovereign',
-      },
-      {
-        icon: '/icons/storage_ic.svg',
-        desc: 'Data Validity',
-        desc1: 'Bitcoin',
-      },
-      {
-        icon: '/icons/storage_ic.svg',
-        desc: 'Data Availability',
-        desc1: 'Polygon',
-      },
-    ],
-  },
-  {
-    key: 'Naka Chain 100',
-    title: 'Naka Chain',
-    srcImg: '/images/defi_chain_img.png',
-    isViewProject: true,
-    isComminSoon: true,
-    childrentList: [
-      {
-        icon: '/icons/layer_ic.svg',
-        desc: 'Use Case',
-        desc1: 'DeFi',
-      },
-      {
-        icon: '/icons/infrastructure_ic.svg',
-        desc: 'Rollups',
-        desc1: 'Optimistic, Sovereign',
-      },
-      {
-        icon: '/icons/storage_ic.svg',
-        desc: 'Data Validity',
-        desc1: 'Bitcoin',
-      },
-      {
-        icon: '/icons/storage_ic.svg',
-        desc: 'Data Availability',
-        desc1: 'Polygon',
-      },
-    ],
-  },
-
-  {
-    key: 'AI Chain 100',
-    title: 'AI Chain',
-    srcImg: '/images/gaming_chain_img.png',
-    isViewProject: true,
-    isComminSoon: true,
-    childrentList: [
-      {
-        icon: '/icons/layer_ic.svg',
-        desc: 'Use Case',
-        desc1: 'AI',
-      },
-      {
-        icon: '/icons/infrastructure_ic.svg',
-        desc: 'Rollups',
-        desc1: 'Optimistic, Sovereign',
-      },
-      {
-        icon: '/icons/storage_ic.svg',
-        desc: 'Data Validity',
-        desc1: 'Bitcoin',
-      },
-      {
-        icon: '/icons/storage_ic.svg',
-        desc: 'Data Availability',
-        desc1: 'Polygon',
+        desc1: 'Celestia',
       },
     ],
   },
 ];
+
+export const DataList = [...DataListOriginal, ...DataListOriginal];
