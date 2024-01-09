536cc01be3840f286410153c68016961b2342897
sett <dr.cat911@generative.xyz>
2024-01-09 17:09:28 +0700

    fx

---
+++ src/modules/landing/Componets/Scalable/index.tsx
@@ -0,0 +1,13 @@
+import s from './style.module.scss';
+import Chars from '@/interactive/Chars';
+import Lines from '@/interactive/Lines';
+
+export default function Scalable() {
+
+  return <div className={s.scalable}>
+
+    <div className={s.scalable_bottom}>
+
+    </div>
+  </div>;
+}
