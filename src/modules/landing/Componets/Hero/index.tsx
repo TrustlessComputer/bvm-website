2a0e0e9e1b4fe00ab61fab79cfa4771d41a91719
sett <dr.cat911@generative.xyz>
2024-01-09 11:19:31 +0700

    feat: landing

---
+++ src/modules/landing/Componets/Hero/index.tsx
@@ -0,0 +1,6 @@
+export default function Hero(){
+
+  return <div>
+    <h2>HERO</h2>
+  </div>
+}
---
+++ src/modules/landing/index.tsx
@@ -0,0 +1,7 @@
+export default Landing function(){
+
+
+  return <div>
+    <h1>LANDING</h1>
+  </div>
+}
