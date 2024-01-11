b0648d979a7ff84362da248f7dddaa063bef6af9
sett <dr.cat911@generative.xyz>
2024-01-11 16:46:27 +0700

    fix

--- src/modules/landing/Componets/ScalableSlide/ScalableItem/index.tsx
+++ src/modules/landing/Componets/ScalableSlide/ScalableItem/index.tsx
@@ -3,6 +3,6 @@ import s from './styles.module.scss';
 
 export default function ScalableItem({layer}:{layer: number}) {
   return <div className={`${s.scalableItem} js-scalableItem ${s[`scalableItem__${layer}`]}`}>
-    <Image className={'js-image'} width={1178} height={773} src={`/landing/compress/slide${layer}.png`} alt={'slide1'} />
+    <Image loading={'eager'} className={'js-image'} width={1178} height={773} src={`/landing/compress/slide${layer}.png`} alt={'slide1'} />
   </div>;
 }
