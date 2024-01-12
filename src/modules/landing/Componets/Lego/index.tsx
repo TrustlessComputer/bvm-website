29976540e2b84f249b330462fbf5997ff37577d5
sett <dr.cat911@generative.xyz>
2024-01-12 16:01:35 +0700

    fx

--- src/modules/landing/Componets/Lego/index.tsx
+++ src/modules/landing/Componets/Lego/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './styles.module.scss';
 import Chars from '@/interactive/Chars';
+import Lines from '@/interactive/Lines';
 import Fade from '@/interactive/Fade';
 import { Button } from '@chakra-ui/react';
 
@@ -9,17 +10,21 @@ export default function Lego() {
     <div className={s.lego}>
       <div className={s.lego_content}>
         <h3 className={s.lego_content_title}>
-          Customize your Bitcoin L2 blockchain with the best-of-breed building
-          blocks.
+          <Chars>
+            Customize your Bitcoin L2 blockchain with <b>the best-of-breed building
+            blocks.</b>
+          </Chars>
         </h3>
 
-        <p className={s.lego_content_description}>
-          Choose a rollup method, select a data availability layer, and then
-          launch to the world — it’s that easy. You can even install default
-          dapps like Uniswap, Compound, and DAO. It’s a new way to build
-          blockchain.
-        </p>
-        <Fade delay={0.5}>
+        <div className={s.lego_content_description}>
+          <Lines delay={.2}>
+            Choose a rollup method, select a data availability layer, and then
+            launch to the world — it’s that easy. You can even install default
+            dapps like Uniswap, Compound, and DAO. It’s a new way to build
+            blockchain.
+          </Lines>
+        </div>
+        <Fade delay={0.4}>
           <Button
             bgColor={'#EF601B'}
             color={'#fff'}
