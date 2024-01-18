975461792b55c30f30b56f17cef1f4b9fd06cb16
0xmegalodon <stephen@newbitcoincity.com>
2024-01-18 15:21:30 +0700

    update interval get prices

--- src/app/private-sale/page.tsx
+++ src/app/private-sale/page.tsx
@@ -1,12 +1,6 @@
-"use client";
-
 import PrivateSaleModule from '@/modules/PrivateSale';
 import MainLayout from '@/layouts/MainLayout';
 import s from './styles.module.scss';
-import LocalStorageUtil from '@/utils/localstorage';
-import { KEY_VC_TYPE } from '@/constants/storage-key';
-import { useEffect } from 'react';
-import { getCacheTokensRate } from '@/services/bitcoin';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -24,26 +18,6 @@ export const metadata: Metadata = {
 };
 
 const PrivateSale = () => {
-  const vcType = LocalStorageUtil.get(KEY_VC_TYPE);
-
-  useEffect(() => {
-    if(!vcType) {
-      LocalStorageUtil.set(KEY_VC_TYPE, 'vc1');
-    }
-  }, [vcType]);
-
-  useEffect(() => {
-    getCacheTokensRate();
-
-    const interval = setInterval(() => {
-      getCacheTokensRate();
-    }, 5 * 60 * 1000);
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
-
   return (
     <MainLayout>
       <div className={s.container}>
--- src/modules/PrivateSale/index.tsx
+++ src/modules/PrivateSale/index.tsx
@@ -1,8 +1,34 @@
+"use client";
+
 import s from './styles.module.scss';
 import {Box} from "@chakra-ui/react";
 import AboveTheFold from "./aboveTheFold";
+import LocalStorageUtil from '@/utils/localstorage';
+import { KEY_VC_TYPE } from '@/constants/storage-key';
+import { useEffect } from 'react';
+import { getCacheTokensRate } from '@/services/bitcoin';
 
 const PrivateSaleModule = () => {
+  const vcType = LocalStorageUtil.get(KEY_VC_TYPE);
+
+  useEffect(() => {
+    if(!vcType) {
+      LocalStorageUtil.set(KEY_VC_TYPE, 'vc1');
+    }
+  }, [vcType]);
+
+  useEffect(() => {
+    getCacheTokensRate();
+
+    const interval = setInterval(() => {
+      getCacheTokensRate();
+    }, 5 * 60 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   return (
     <Box className={s.container}>
       <AboveTheFold />
