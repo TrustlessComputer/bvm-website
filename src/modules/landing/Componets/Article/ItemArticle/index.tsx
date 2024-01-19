f25b9c05fa87b6926a1b0cae79ff4b2800d5137b
sett <dr.cat911@generative.xyz>
2024-01-19 11:55:46 +0700

    fx

--- src/modules/landing/Componets/Article/ItemArticle/index.tsx
+++ src/modules/landing/Componets/Article/ItemArticle/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import s from './styles.module.scss';
-import { IBlog } from '../../Section_7/constant';
+import { IBlog, LOGOS } from '../../Section_7/constant';
 import { Image } from '@chakra-ui/react';
 import useWindowSize from '@/hooks/useWindowSize';
 
 export default function ItemArticle({ data }: { data: IBlog }) {
   const { mobileScreen } = useWindowSize();
+  const getLogo = useMemo((): string => {
+    const tmp = LOGOS.filter((itemLogo => {
+      return itemLogo.id === data.logo;
+    }));
+    return tmp.length ? tmp[0].img : '';
+  }, [data]);
   return (
     <div className={s.itemArticle}>
       <Image
@@ -15,6 +21,17 @@ export default function ItemArticle({ data }: { data: IBlog }) {
         width={'100%'}
         height={mobileScreen ? 'auto' : '250px'}
       />
+      {
+        getLogo && <div className={s.cardLogo}>
+          <Image
+            src={getLogo}
+            alt='thumb image'
+            width={'100%'}
+            height={28}
+            objectFit={'contain'}
+          />
+        </div>
+      }
       <div className={s.itemArticle_content}>
         <h5 className={s.itemArticle_content_title}>{data.title}</h5>
         <p className={s.itemArticle_content_description}>{data.desc}</p>
