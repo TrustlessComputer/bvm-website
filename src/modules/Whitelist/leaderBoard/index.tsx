c8ac520a78ab2394ad007570d53532dd3642a006
camewell <130561684+camewell071@users.noreply.github.com>
2024-01-23 15:06:53 +0700

    chore: update code

--- src/modules/Whitelist/leaderBoard/index.tsx
+++ src/modules/Whitelist/leaderBoard/index.tsx
@@ -461,7 +461,7 @@ const LeaderBoard = (props: IProps) => {
               label={
                 <Flex direction="column" color="black" opacity={0.7}>
                   <p>
-                    ETH you stake on <strong>Blast</strong> or the more <strong>Optimism (OP)</strong>, <strong>Base (BASE)</strong>, or <strong>Arbitrum (ARB)</strong> tokens you hold.
+                    L2 points are calculated from your <strong>Optimism</strong>, <strong>Base</strong>, <strong>Arbitrum</strong> holding, and <strong>Blast</strong> staking.
                   </p>
                 </Flex>
               }
