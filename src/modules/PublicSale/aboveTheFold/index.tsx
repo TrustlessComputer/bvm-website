bce79985abda8b1c5534a5a88b912bc8244e1da8
0xmegalodon <stephen@newbitcoincity.com>
2024-01-23 14:11:51 +0700

    add public sale page

---
+++ src/modules/PublicSale/aboveTheFold/index.tsx
@@ -0,0 +1,51 @@
+import s from './styles.module.scss';
+import { Box, Button, Flex, SimpleGrid, Text, useSteps } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import Lines from '@/interactive/Lines';
+import Fade from '@/interactive/Fade';
+import useAnimationStore from '@/stores/useAnimationStore';
+import BuyForm from '@/modules/PublicSale/BuyForm';
+import HeadingTextTyping from '@/modules/landing/Componets/HeadingTextTyping';
+import LocalStorageUtil from '@/utils/localstorage';
+import { KEY_VC_TYPE } from '@/constants/storage-key';
+import { getVCInformation } from '@/services/player-share';
+import { VCInfo } from '@/interfaces/vc';
+import LeaderBoard from '@/modules/PublicSale/leaderBoard';
+
+const AboveTheFold = () => {
+  const { setPlay } = useAnimationStore();
+  const vcType = LocalStorageUtil.get(KEY_VC_TYPE);
+  const [vcInfo, setVCInfo] = useState<VCInfo>();
+
+  useEffect(() => {
+    setTimeout(setPlay, 400);
+  }, []);
+
+  useEffect(() => {
+    if(vcType) {
+      getVCInfo(vcType as string);
+    }
+  }, [vcType]);
+
+  const getVCInfo = async (id: string) => {
+    const res = await getVCInformation({ vc_type: id });
+    setVCInfo(res);
+  }
+
+  return (
+    <Flex direction={"column"} justifyContent={"space-between"} className={s.container} bgImg={`/private-sale/bg.webp`}>
+      <SimpleGrid className={`container ${s.content}`} gridTemplateColumns={["1fr", "1.25fr 1fr"]} gap={[6, 0]}>
+        <Flex className={s.leftSection} direction={"column"} gap={[6, 6]} justifyContent={"flex-start"}>
+          <LeaderBoard />
+        </Flex>
+        <Flex className={s.rightSection} justifyContent={["center", "flex-end"]} alignItems={"flex-end"}>
+          <Flex className={s.btnJoinWrapper} direction={"column"}>
+            <BuyForm vcInfo={vcInfo}/>
+          </Flex>
+        </Flex>
+      </SimpleGrid>
+    </Flex>
+  );
+};
+
+export default AboveTheFold;
