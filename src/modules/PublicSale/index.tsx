711ad776b7eb1d77e34c5c815a97efc30a2cb6eb
0xmegalodon <stephen@newbitcoincity.com>
2024-01-25 16:24:39 +0700

    add top header

--- src/modules/PublicSale/index.tsx
+++ src/modules/PublicSale/index.tsx
@@ -3,11 +3,13 @@
 import s from './styles.module.scss';
 import { Box } from '@chakra-ui/react';
 import AboveTheFold from './aboveTheFold';
+import Allocation from '@/modules/bvm_v2/Allocation';
 
 const PublicSaleModule = () => {
   return (
     <Box className={s.container}>
       <AboveTheFold />
+      <Allocation />
     </Box>
   );
 };
