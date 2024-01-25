e0f50ef647588e49546d093c92d41f017687c799
0xmegalodon <stephen@newbitcoincity.com>
2024-01-25 18:31:09 +0700

    update code

--- src/modules/PublicSale/aboveTheFold/index.tsx
+++ src/modules/PublicSale/aboveTheFold/index.tsx
@@ -1,21 +1,18 @@
 import s from './styles.module.scss';
-import { Box, Button, Flex, SimpleGrid, Text, useSteps } from '@chakra-ui/react';
+import { Flex, SimpleGrid } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
-import Lines from '@/interactive/Lines';
-import Fade from '@/interactive/Fade';
 import useAnimationStore from '@/stores/useAnimationStore';
 import BuyForm from '@/modules/PublicSale/BuyForm';
-import HeadingTextTyping from '@/modules/landing/Componets/HeadingTextTyping';
 import LocalStorageUtil from '@/utils/localstorage';
 import { KEY_VC_TYPE } from '@/constants/storage-key';
 import { getVCInformation } from '@/services/player-share';
 import { VCInfo } from '@/interfaces/vc';
-import LeaderBoard from '@/modules/PublicSale/leaderBoard';
 import Welcome from '@/modules/PublicSale/welcome';
 import Playgame from '@/modules/PublicSale/playGame';
 import AddMoreContribution from '@/modules/PublicSale/addMoreContribution';
 import TopHeader from '@/modules/PublicSale/topHeader';
 import LeaderBoardVisual from '@/modules/PublicSale/leaderBoardVisual';
+import RaffleButton from '@/modules/PublicSale/raffleButton';
 
 const AboveTheFold = () => {
   const { setPlay } = useAnimationStore();
@@ -44,6 +41,7 @@ const AboveTheFold = () => {
           <TopHeader />
           <LeaderBoardVisual />
           <AddMoreContribution />
+          <RaffleButton className={s.raffleButton}/>
         </Flex>
         <Flex className={s.rightSection} direction={"column"}>
           <BuyForm vcInfo={vcInfo}/>
