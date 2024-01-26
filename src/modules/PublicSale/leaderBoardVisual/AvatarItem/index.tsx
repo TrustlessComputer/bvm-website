83666a70e5d374497291dfbfecd927f3be4825da
0xmegalodon <stephen@newbitcoincity.com>
2024-01-26 15:25:42 +0700

    add contribute modal

--- src/modules/PublicSale/leaderBoardVisual/AvatarItem/index.tsx
+++ src/modules/PublicSale/leaderBoardVisual/AvatarItem/index.tsx
@@ -1,9 +1,15 @@
 import s from './styles.module.scss';
 import { getUrlAvatarTwitter } from '@/utils/twitter';
-import React, { ReactElement, useState } from 'react';
+import React, { forwardRef, ReactElement, useState } from 'react';
 import Image from 'next/image';
+import { ILeaderBoardPoint } from '@/interfaces/leader-board-point';
 
-export default function AvatarItem({ data }: any) {
+interface IProps {
+  data: ILeaderBoardPoint
+}
+
+const AvatarItem = forwardRef((props: IProps, ref: any) => {
+  const { data, ...rest } = props;
 
   const [error, setError] = useState<boolean>(false);
   const PlaceImage = (): ReactElement => {
@@ -13,30 +19,34 @@ export default function AvatarItem({ data }: any) {
       src={'/images/mk-user.jpg'} alt={'user'} />;
   };
 
-  return <div
-    className={`${s.avatarItem} ${data.levelRender !== undefined && 'level-' + data.levelRender} js-avatarItem`}>
-    <div className={s.avatarItem_inner}>
-      <div
-        className={s.avatarItem_avatar}
-        onClick={() => {
-          window.open(`https://twitter.com/${data?.twitter_username}`);
-        }}
-      >
-        {
-          error && <PlaceImage />
-        }
-        {!error && <Image
-          width={120}
-          height={120}
-          onError={(e) => {
-            setError(true);
+  return (
+    <div
+      className={`${s.avatarItem} ${data.levelRender !== undefined && 'level-' + data.levelRender} js-avatarItem`} ref={ref} {...rest}>
+      <div className={s.avatarItem_inner}>
+        <div
+          className={s.avatarItem_avatar}
+          onClick={() => {
+            window.open(`https://twitter.com/${data?.twitter_username}`);
           }}
-          src={getUrlAvatarTwitter(
-            data?.twitter_avatar as string,
-            'medium',
-          ) || ''} alt={'medium'} />}
+        >
+          {
+            error && <PlaceImage />
+          }
+          {!error && <Image
+            width={120}
+            height={120}
+            onError={(e) => {
+              setError(true);
+            }}
+            src={getUrlAvatarTwitter(
+              data?.twitter_avatar as string,
+              'medium',
+            ) || ''} alt={'medium'} />}
+        </div>
+        <p className={s.price}>$100,000</p>
       </div>
-      <p className={s.price}>$100,000</p>
-    </div>
-  </div>;
-}
+  </div>
+  );
+});
+
+export default AvatarItem;
--- src/modules/PublicSale/leaderBoardVisual/index.tsx
+++ src/modules/PublicSale/leaderBoardVisual/index.tsx
@@ -14,6 +14,9 @@ import AnimatedText from '@/modules/PublicSale/leaderBoardVisual/FloatTexts';
 import Image from 'next/image';
 import AvatarYou from '@/modules/PublicSale/leaderBoardVisual/AvatarYou';
 import AddMoreContribution from '@/modules/PublicSale/addMoreContribution';
+import { Flex, Text, Tooltip } from '@chakra-ui/react';
+import s from '@/modules/PublicSale/BuyForm/styles.module.scss';
+import HorizontalItem from '@/components/HorizontalItem';
 
 const valueToClassName: any = {
   '10': 'boost_10',
@@ -39,7 +42,6 @@ const LeaderBoardVisual = (props: IProps) => {
   const dispatch = useAppDispatch();
   const emptyArray: number[] = Array.from({ length: (13 * 5) });
   const indexUserInsert = Math.floor(Math.random() * emptyArray.length);
-
 
   const hasIncrementedPageRef = useRef(false);
   const refParams = useRef({
@@ -127,6 +129,35 @@ const LeaderBoardVisual = (props: IProps) => {
 
   }, [list]);
 
+  const ContributorInfo = () => {
+    return (
+      <Flex direction={'column'} w={'284px'} gap={4} className={s.contributorInfo}>
+        <HorizontalItem className={s.rowData} label={'USER'} value={'clinkzchan'} />
+        <HorizontalItem className={s.rowData} label={'RANK'} value={'1,000'} />
+        <HorizontalItem className={s.rowData} label={'CONTRIBUTION'} value={'$120,000'} />
+        <HorizontalItem className={s.rowData} label={'ALLOCATION'} value={<Text color={'#FF5312'}>15 BVM</Text>} />
+        <HorizontalItem className={s.rowData} label={'BOOST'} value={
+          <Flex gap={1} alignItems={'center'}>
+            <svg width='14' height='20' viewBox='0 0 14 20' fill='none'
+                 xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M13.6663 8.18093H8.21179L9.42391 0.908203L0.333008 11.8173H5.78755L4.57543 19.09L13.6663 8.18093Z'
+                fill='url(#paint0_linear_29823_6261)' />
+              <defs>
+                <linearGradient id='paint0_linear_29823_6261' x1='0.333008' y1='9.99911' x2='13.6663'
+                                y2='9.99911' gradientUnits='userSpaceOnUse'>
+                  <stop stop-color='#007659' />
+                  <stop offset='1' stop-color='#35CCA6' />
+                </linearGradient>
+              </defs>
+            </svg>
+            <Text fontSize={'16px'} fontWeight={'500'} className={s.boostLight}>10%</Text>
+          </Flex>
+        } />
+      </Flex>
+    );
+  };
+
   return (
     <div className={styles.container} id={LEADER_BOARD_ID}>
       <ScrollWrapper
@@ -140,7 +171,18 @@ const LeaderBoardVisual = (props: IProps) => {
         {
           listRender.slice(0, 23).map((item, index) => {
             return <>
+            <Tooltip minW='220px'
+                     bg='white'
+                     boxShadow='rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;'
+                     borderRadius='4px'
+                     padding='16px'
+                     hasArrow
+                     label={
+                       <ContributorInfo />
+                     }
+            >
               <AvatarItem data={item} />
+            </Tooltip>
               {
                 item.lastRender && <span className={styles.lastRender}></span>
               }
