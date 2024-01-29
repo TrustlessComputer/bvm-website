d17327163ab4337624af1fcc2d28f2abfae28be3
camewell <130561684+camewell071@users.noreply.github.com>
2024-01-29 20:35:15 +0700

    chore: update ui

--- src/modules/bvm_v2/Vesting/index.tsx
+++ src/modules/bvm_v2/Vesting/index.tsx
@@ -3,35 +3,14 @@ import React from 'react';
 import s from './Vesting.module.scss';
 import BoxContent from '@/layouts/BoxContent';
 
-type Props = {};
-
-const Vesting = (props: Props) => {
+const Vesting = () => {
   return (
     <Box
       className={s.wrapper}
-      py={{ base: '40px', md: '100px' }}
       position={'relative'}
     >
-      <Box
-        bgColor={'#000'}
-        w="100vw"
-        position={'absolute'}
-        top={0}
-        left={'calc(-50vw + 50%)'}
-        h="100%"
-        zIndex={0}
-      ></Box>
-      <Box className={s.header} zIndex={1} position={'relative'}>
-        <Text as="h4" className={s.heading}>
-          BVM Vesting
-        </Text>
-        <Text className={s.desc} maxW={'63ch'} mx={'auto'}>
-          Tokens are subject to the following vesting schedule, which aims to
-          minimize the negative impacts of vesting unlocks and better align
-          incentives.
-        </Text>
-      </Box>
       <Box className={s.table_wrapper}>
+        <Text mb={{ base: "32px", lg: "60px" }} fontSize="24px">BVM Vesting 5 Year Schedule</Text>
         <table className={s.vesting_table}>
           <thead>
             <tr>
