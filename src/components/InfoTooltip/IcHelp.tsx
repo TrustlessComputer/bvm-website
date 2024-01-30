930c96ce3948c681fb38cf2222c99ab89b293ceb
0xmegalodon <stephen@newbitcoincity.com>
2024-01-30 19:09:54 +0700

    add tooltip & login

---
+++ src/components/InfoTooltip/IcHelp.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { Image } from '@chakra-ui/react';
+import React from 'react';
+
+const IcHelp = () => {
+  return <Image src={`/icons/ic-tooltip.svg`} />;
+};
+
+export default IcHelp;
