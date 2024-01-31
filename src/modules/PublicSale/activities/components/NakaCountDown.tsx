16015f233cbc1fabd679025690d22ffd5114395c
camewell <130561684+camewell071@users.noreply.github.com>
2024-01-31 11:23:59 +0700

    chore: update ui

--- src/modules/PublicSale/activities/components/NakaCountDown.tsx
+++ src/modules/PublicSale/activities/components/NakaCountDown.tsx
@@ -7,9 +7,14 @@ import Countdown from '@/modules/Whitelist/stepAirdrop/Countdown';
 import SvgInset from '@/components/SvgInset';
 import { CDN_URL_ICONS } from '@/config';
 import { formatCurrency, formatString } from '@/utils/format';
+import BigNumber from 'bignumber.js';
+import { useAppSelector } from '@/stores/hooks';
+import { coinPricesSelector } from '@/stores/states/common/selector';
+import { Coin } from '@/stores/states/common/types';
 
 const NakaCountDown = React.memo(() => {
   const { bestPNL } = useTradeNakaData();
+  const btcPrice = useAppSelector(coinPricesSelector)?.[Coin.BTC];
   if (!bestPNL || !bestPNL.to_time || !bestPNL.from_time) return <></>;
 
   return (
@@ -34,7 +39,7 @@ const NakaCountDown = React.memo(() => {
             if (!bestPNL?.winner?.twitter_username) return;
             window.open(`https://twitter.com/${bestPNL?.winner?.twitter_username}`, '_blank');
           }}>
-            {formatString(bestPNL?.winner?.twitter_name || bestPNL?.winner?.address , 6)} <span>+{formatCurrency(bestPNL?.winner?.realized_pnl || '0', 0, 5)} BTC</span>
+            {formatString(bestPNL?.winner?.twitter_name || bestPNL?.winner?.address , 6)} <span>+${formatCurrency(new BigNumber(bestPNL?.winner?.realized_pnl || '0').times(btcPrice).toNumber(), 0, 4)}</span>
           </Text>
         </Flex>
       ) : (
