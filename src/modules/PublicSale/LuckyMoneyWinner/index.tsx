008b73fa02b6376a5ee58054a05302a1f6785535
camewell <130561684+camewell071@users.noreply.github.com>
2024-02-03 05:31:10 +0700

    chore: update code

--- src/modules/PublicSale/LuckyMoneyWinner/index.tsx
+++ src/modules/PublicSale/LuckyMoneyWinner/index.tsx
@@ -8,7 +8,7 @@ import background from '@/public/images/lucky-money-envelops/lucky-money-winner-
 import Avatar from '@/components/Avatar';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { settingMomentFromNow } from '@/utils/helpers';
-import { formatString } from '@/utils/format';
+import { formatNameOrAddress, formatString } from '@/utils/format';
 
 dayjs.extend(relativeTime);
 
@@ -44,7 +44,7 @@ const LuckyMoneyWinner = () => {
         </div>
         <div>
           <div className={s.textMessage}>
-            <strong>{formatString(winner?.user.twitter_name || '---', 6, '')}</strong> just snatched
+            <strong>{formatNameOrAddress(winner?.user.twitter_name || '---')}</strong> just snatched
             a red packet successfully and{' '}
             <strong>got {winner?.bvm_amount} BVM</strong>.
           </div>
