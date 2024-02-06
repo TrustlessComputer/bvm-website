e0567d498e7e632f9de809a935475a4f4590b452
camewell <130561684+camewell071@users.noreply.github.com>
2024-02-06 11:53:11 +0700

    chore: update ui

--- src/layouts/Header/menuConfig.ts
+++ src/layouts/Header/menuConfig.ts
@@ -93,16 +93,16 @@ export const NAV_ITEMS_LEFT: Array<NavItem> = [
     isHide: false,
   },
   {
-    label: 'Module Store',
-    href: '/module-store',
-    isNewWindow: false,
-    isHide: false,
-  },
-  {
     label: checkIsPublicSale() ? 'Public Sale' : 'Launchpad',
     href: checkIsPublicSale() ? '/public-sale' : '/launchpad',
     isNewWindow: false,
-    isStrong: checkIsPublicSale(),
+    isStrong: false,
+    isHide: false,
+  },
+  {
+    label: 'Module Store',
+    href: '/module-store',
+    isNewWindow: false,
     isHide: false,
   },
 ].filter((item) => !item.isHide);
