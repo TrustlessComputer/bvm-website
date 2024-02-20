2b9008c8ec925851bb5758f2cee1d892f349ec2b
sett <dr.cat911@generative.xyz>
2024-02-20 14:24:54 +0700

    feat: builder

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import MainLayout from '@/layouts/MainLayout';
+import Landing from '@/modules/landing';
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <Landing />
+    </MainLayout>
+  );
+}
