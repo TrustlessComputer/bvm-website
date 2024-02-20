0e51cfe9d624a40b0238201c379d1ae8b489cd66
0xmegalodon <stephen@newbitcoincity.com>
2024-02-20 11:54:31 +0700

    add missing module

--- src/modules/Launchpad/index.tsx
+++ src/modules/Launchpad/index.tsx
@@ -4,12 +4,17 @@ import { Box } from '@chakra-ui/react';
 import s from './styles.module.scss';
 import TopContent from '@/modules/Launchpad/topContent';
 import AboveTheFold from '@/modules/Launchpad/aboveTheFold';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { getLaunchpadDetail } from '@/services/launchpad';
 import { useParams } from 'next/navigation';
 import { useSelector } from 'react-redux';
 import { commonSelector } from '@/stores/states/common/selector';
 import { useLaunchpadContext } from '@/Providers/LaunchpadProvider/hooks/useLaunchpadContext';
+import IntroVideos from '@/modules/PublicSale/IntroVideos';
+import RoadmapModule from '@/modules/PublicSale/roadmap';
+import Allocation from '@/modules/bvm_v2/Allocation';
+import FAQContent from '@/modules/Whitelist/FAQContent';
+import LuckyMoney from '@/modules/PublicSale/luckyMoney';
 
 const LaunchpadDetailModule = () => {
   const params = useParams();
@@ -35,6 +40,11 @@ const LaunchpadDetailModule = () => {
         <TopContent />
         <AboveTheFold />
       </Box>
+      <IntroVideos />
+      <RoadmapModule />
+      <Allocation />
+      <FAQContent />
+      <LuckyMoney />
     </Box>
   )
 };
