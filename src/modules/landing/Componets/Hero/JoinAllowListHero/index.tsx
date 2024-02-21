8f78febbab25679ff50a48900224fedaae028674
sett <dr.cat911@generative.xyz>
2024-02-21 11:53:26 +0700

    fix: update hero

--- src/modules/landing/Componets/Hero/JoinAllowListHero/index.tsx
+++ src/modules/landing/Componets/Hero/JoinAllowListHero/index.tsx
@@ -9,33 +9,13 @@ const DELAY = 6.5;
 const JoinAllowListHero = ({ isFooter }: { isFooter?: boolean }) => {
 
   const delay = isFooter ? 0 : DELAY;
-  const [isOpen, setOpen] = useState(false);
 
   return (
 
     <div className={`${s.container} ${isFooter && s.isFooter}`}>
       <div className={`container ${s.content}`}>
         <HeroLabel />
-        <Flex gap={5} flexDirection={'column'}>
-          <Fade delay={delay + .4}>
-            <div>
-              <a href={'#'} onClick={() => setOpen(true)} style={{ textAlign: 'center', display: 'block' }}>
-                <img src={`/public-sale/btn-play-4.png`} width={224} alt={'right'}
-                     style={{ margin: 'auto', marginBottom: '8px' }} />
-                <span style={{ fontSize: '14px', fontWeight: 400 }}>What is BVM?</span>
-              </a>
-            </div>
-          </Fade>
-        </Flex>
       </div>
-      <ModalVideo
-        channel='custom'
-        url={'/public-sale/public_sale_video_2.mp4'}
-        isOpen={isOpen}
-        onClose={() => {
-          setOpen(false);
-        }}
-      />
     </div>
 
   );
