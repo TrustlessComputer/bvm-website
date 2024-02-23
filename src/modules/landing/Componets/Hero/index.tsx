43e53841557862d05e895f86d47d3b3a2b75ff92
Archaon <archaon1574@trustless.computer>
2024-02-23 20:26:51 +0700

    feat: update banner

--- src/modules/landing/Componets/Hero/index.tsx
+++ src/modules/landing/Componets/Hero/index.tsx
@@ -6,14 +6,15 @@ import HeroContent from '@/modules/landing/Componets/Hero/Content';
 import JoinAllowList from '@/modules/landing/Componets/Hero/JoinAllowList';
 import React from 'react';
 import JoinAllowListHero from '@/modules/landing/Componets/Hero/JoinAllowListHero';
+import Banner from './Banner';
 
 export default function Hero() {
   return (
-
     <>
       <div className={s.hero}>
         <div className={s.hero_wrap}>
           <BgHero />
+          <Banner />
         </div>
         <HeroContent />
         <JoinAllowListHero />
