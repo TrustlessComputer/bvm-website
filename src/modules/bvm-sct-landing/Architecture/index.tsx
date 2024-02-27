ec78f1efeb95426e7f2727b252396ad73e59aa0c
sett <dr.cat911@generative.xyz>
2024-02-27 14:10:10 +0700

    fx

--- src/modules/bvm-sct-landing/Architecture/index.tsx
+++ src/modules/bvm-sct-landing/Architecture/index.tsx
@@ -1,4 +1,4 @@
-import s from './styles.module.scss'
+import s from './styles.module.scss';
 import HeadingSection from '@/modules/landing/Componets/HeadingSection';
 import React from 'react';
 import Fade from '@/interactive/Fade';
@@ -7,18 +7,18 @@ const Architecture = (): React.JSX.Element => {
   return (
     <div className={`${s.architectureWrapper}`}>
       <div className={'container'}>
-        <Fade delay={1.2}>
-          <HeadingSection className={s.heading}>
-            Architecture
-          </HeadingSection>
-        </Fade>
-        <Fade delay={1.2}>
-          <p className={s.content}>BVM is a state machine similar to Ethereum-VM that utilizes the Bitcoin blockchain as a
+        <HeadingSection className={s.heading}>
+          Architecture
+        </HeadingSection>
+        <Fade>
+          <p className={s.content}>BVM is a state machine similar to Ethereum-VM that utilizes the Bitcoin blockchain as
+            a
             data layer to achieve transaction-level consensus. This approach allows BVM to function as a general-purpose
             state machine while taking advantage of the Bitcoin blockchain's security and data availability without
             requiring additional modules, such as network or consensus protocols.
           </p>
-          <p className={s.content}>There are 4 most important components needed to explain: the local mempool, Ethereum-like VM, TxWriter, and
+          <p className={s.content}>There are 4 most important components needed to explain: the local mempool,
+            Ethereum-like VM, TxWriter, and
             TxReader.</p>
         </Fade>
       </div>
