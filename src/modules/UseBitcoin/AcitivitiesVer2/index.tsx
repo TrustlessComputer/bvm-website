1c3b6f16b31276c6d9c9d8da0c55e49c0ab81fd0
0xmegalodon <stephen@newbitcoincity.com>
2024-02-28 10:58:31 +0700

    update UI

--- src/modules/UseBitcoin/AcitivitiesVer2/index.tsx
+++ src/modules/UseBitcoin/AcitivitiesVer2/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Box, Flex, Image, SimpleGrid, Text, Tooltip, useDisclosure } from '@chakra-ui/react';
-import styles from './styles.module.scss';
-import IncreaseNumber from '@/modules/PublicSale/activities/components/IncreaseNumber';
+import { Box, Flex, GridItem, Image, SimpleGrid, Text, Tooltip, useDisclosure } from '@chakra-ui/react';
+import s from './styles.module.scss';
 import { useAppSelector } from '@/stores/hooks';
 import { numberReportSelector } from '@/stores/states/activities/selector';
 import { formatCurrency } from '@/utils/format';
@@ -9,7 +8,6 @@ import BigNumber from 'bignumber.js';
 import { coinPricesSelector } from '@/stores/states/common/selector';
 import { Coin } from '@/stores/states/common/types';
 import cx from 'clsx';
-import { isMobile } from 'react-device-detect';
 
 interface ICTA {
   title: string;
@@ -24,7 +22,9 @@ enum ActivityType {
   Modular,
   Social,
   AI
-}
+};
+
+const ActivityKey = ['game', 'naka', 'modular', 'social', 'ai'];
 
 const MODULAR_TW_LINK = 'https://twitter.com/BVMnetwork/status/1752952381007171646';
 
@@ -54,8 +54,8 @@ export const NormalRow = (p: { key: string, value?: string, mask?: boolean }) =>
       `
         <li>
           <div style='display: flex; flex-direction: row'>
-            <span style='font-size: 14px; line-height: 160%; color: black; font-weight: 700'>${p.key}</span>
-            <div style='background-color: black; opacity: 0.3; width: 152px; height: 22px; margin-left: 8px' />
+            <span style='font-size: 14px; line-height: 160%; color: white; font-weight: 700'>${p.key}</span>
+            <div style='background-color: white; opacity: 0.3; width: 152px; height: 22px; margin-left: 8px' />
           </div
         </li>
       `
@@ -67,7 +67,7 @@ export const NormalRow = (p: { key: string, value?: string, mask?: boolean }) =>
       `
         <li>
           <p style='font-size: 14px; line-height: 160%'>
-            <span style='color: black; font-weight: 400; opacity: 0.7;'>${p.value}</span>
+            <span style='color: white; font-weight: 400; opacity: 0.7;'>${p.value}</span>
           </p>
         </li>
       `
@@ -78,8 +78,8 @@ export const NormalRow = (p: { key: string, value?: string, mask?: boolean }) =>
     `
       <li>
         <p style='font-size: 14px; line-height: 160%'>
-          <span style='color: black; font-weight: 700'>${p.key}</span>
-          <span style='color: black; font-weight: 400; opacity: 0.7; margin-left: 4px;'>${p.value}</span>
+          <span style='color: white; font-weight: 700'>${p.key}</span>
+          <span style='color: white; font-weight: 400; opacity: 0.7; margin-left: 4px;'>${p.value}</span>
         </p>
       </li>
     `
@@ -91,9 +91,9 @@ export const LinkRow = (p: { key: string, value: string, link: string, isSpecial
     `
     <li>
       <div style='display: flex; flex-direction: row; align-items: end'>
-        <span style='font-size: 14px; line-height: 160%; color: black; font-weight: 700'>${p.key}</span>
+        <span style='font-size: 14px; line-height: 160%; color: white; font-weight: 700'>${p.key}</span>
         <a href='${p.link}' target='_blank'
-           style='display: flex; flex-direction: row; gap: 8px; align-items: center; font-size: 14px; color: #FA4E0E; font-weight: 400; margin-left: 8px;'>
+           style='display: flex; flex-direction: row; gap: 8px; align-items: center; font-size: 14px; color: #FFA888; font-weight: 400; margin-left: 8px;'>
           ${p.value}
           ${p?.isSpecial ? (
       `<svg width='11' height='11' viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg'>
@@ -105,7 +105,7 @@ export const LinkRow = (p: { key: string, value: string, link: string, isSpecial
       `<svg width='11' height='11' viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg'>
         <path
           d='M3.7335 2.35074C3.63267 2.35429 3.53216 2.33751 3.43796 2.30138C3.34376 2.26525 3.2578 2.21052 3.18521 2.14045C3.11263 2.07038 3.0549 1.9864 3.01547 1.89354C2.97604 1.80067 2.95571 1.70081 2.95572 1.59992C2.95572 1.49903 2.97604 1.39917 3.01547 1.30631C3.0549 1.21344 3.11263 1.12947 3.18521 1.0594C3.2578 0.989326 3.34376 0.934593 3.43796 0.898464C3.53216 0.862335 3.63267 0.845549 3.7335 0.849108L9.40002 0.849108C9.59911 0.849232 9.79001 0.928376 9.93079 1.06915C10.0716 1.20993 10.1507 1.40083 10.1508 1.59992L10.1508 7.26644C10.1544 7.36727 10.1376 7.46778 10.1015 7.56198C10.0653 7.65618 10.0106 7.74214 9.94054 7.81473C9.87047 7.88731 9.7865 7.94505 9.69363 7.98448C9.60077 8.0239 9.50091 8.04423 9.40002 8.04423C9.29913 8.04423 9.19927 8.0239 9.1064 7.98447C9.01354 7.94504 8.92956 7.88731 8.85949 7.81473C8.78942 7.74214 8.73469 7.65618 8.69856 7.56198C8.66243 7.46778 8.64565 7.36727 8.64921 7.26644L8.6492 3.41321L1.78563 10.2768C1.64474 10.4177 1.45365 10.4968 1.2544 10.4968C1.05514 10.4968 0.864054 10.4177 0.723161 10.2768C0.582269 10.1359 0.503116 9.9448 0.503115 9.74554C0.503116 9.54629 0.582268 9.3552 0.723161 9.21431L7.58673 2.35074L3.7335 2.35074Z'
-          fill='#FA4E0E' />
+          fill='#FFA888' />
       </svg>`
     )
     }
@@ -118,9 +118,9 @@ export const LinkRow = (p: { key: string, value: string, link: string, isSpecial
 
 export const ReportRow = (p: { key: string, value: string, prefix?: string, maxDigit?: number, diffNumb?: number }) => {
   return (
-    <Flex flexDir='row' alignItems='end' color='black'>
+    <Flex flexDir='row' alignItems='end' color='white'>
       {p.prefix && (
-        <span style={{ fontWeight: '500', fontSize: '16px', lineHeight: '140%' }}>
+        <span style={{ fontWeight: '500', fontSize: '12px', lineHeight: '140%' }}>
           {p.prefix || ''}
         </span>
       )}
@@ -138,10 +138,10 @@ export const ReportRow = (p: { key: string, value: string, prefix?: string, maxD
           return formatCurrency(_value, 0, p.maxDigit || 0);
         }}
       />*/}
-      <Text fontSize='16px' lineHeight='140%' color='black' fontWeight='500'>
+      <Text fontSize='12px' lineHeight='140%' color='white' fontWeight='500'>
         {formatCurrency(new BigNumber(new BigNumber(p.value).toFixed(0, BigNumber.ROUND_CEIL)).toNumber(), 0, 0, 'BTC', false)}
       </Text>
-      <Text fontSize='16px' lineHeight='120%' color='black' opacity={0.7} fontWeight='400' ml='4px'>
+      <Text fontSize='12px' lineHeight='120%' color='white' opacity={0.7} fontWeight='400' ml='4px'>
         {p.key}
       </Text>
     </Flex>
@@ -159,18 +159,16 @@ const ActivitiesVer2 = React.memo(() => {
       {
         title: 'GameFi on Bitcoin',
         desc: `
-          <div style='display: flex; flex-direction: ${isMobile ? "column" : "row"};justify-content: space-between; color: black'>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
+            <ul style='color: white'>
               ${LinkRow({ key: 'Bitcoin L2:', value: 'Bitcoin Arcade', link: GAME_LINK.ARCA })}
               ${NormalRow({ key: 'Block time:', value: '2 seconds' })}
-              ${NormalRow({ key: 'Data availability layer:', value: 'Bitcoin + Polygon' })}
-            </ul>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
               ${NormalRow({ key: 'Network type:', value: 'Bitcoin Testnet' })}
               ${NormalRow({ key: 'Deployer:', value: 'Arcade team' })}
+              ${NormalRow({ key: 'Rollups:', value: 'Optimistic' })}
+              ${NormalRow({ key: 'Data Validity:', value: 'Bitcoin' })}
+              ${NormalRow({ key: 'Data Availability:', value: 'Polygon' })}
               ${NormalRow({ key: 'Launch date:', value: 'January 01, 2024' })}
             </ul>
-          </div>
         `,
         banner: 'banner-01_1.png',
         link: GAME_LINK.ARCA,
@@ -179,18 +177,16 @@ const ActivitiesVer2 = React.memo(() => {
       {
         title: 'DeFi on Bitcoin',
         desc: `
-          <div style='display: flex; flex-direction: ${isMobile ? "column" : "row"};justify-content: space-between; color: black'>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
+            <ul style='color: white'>
               ${LinkRow({ key: 'Bitcoin L2:', value: 'Naka', link: GAME_LINK.NAKA })}
               ${NormalRow({ key: 'Block time:', value: '2 seconds' })}
-              ${NormalRow({ key: 'Data availability layer:', value: 'Bitcoin + Polygon' })}
-            </ul>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
               ${NormalRow({ key: 'Network type:', value: 'Bitcoin Mainnet' })}
               ${NormalRow({ key: 'Deployer:', value: 'Naka team' })}
+              ${NormalRow({ key: 'Rollups:', value: 'Optimistic' })}
+              ${NormalRow({ key: 'Data Validity:', value: 'Bitcoin' })}
+              ${NormalRow({ key: 'Data Availability:', value: 'Polygon' })}
               ${NormalRow({ key: 'Launch date:', value: 'January 01, 2024' })}
             </ul>
-          </div>
         `,
         banner: 'banner-02.png',
         link: GAME_LINK.NAKA,
@@ -199,18 +195,16 @@ const ActivitiesVer2 = React.memo(() => {
       {
         title: 'SocialFi on Bitcoin',
         desc: `
-          <div style='display: flex; flex-direction: ${isMobile ? "column" : "row"};justify-content: space-between; color: black'>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
+            <ul style='color: white'>
               ${LinkRow({ key: 'Bitcoin L2:', value: 'Alpha', link: GAME_LINK.ALPHA })}
               ${NormalRow({ key: 'Block time:', value: '2 seconds' })}
-              ${NormalRow({ key: 'Data availability layer:', value: 'Bitcoin + Polygon' })}
-            </ul>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
               ${NormalRow({ key: 'Network type:', value: 'Bitcoin Mainnet' })}
               ${NormalRow({ key: 'Deployer:', value: 'NBC team' })}
+              ${NormalRow({ key: 'Rollups:', value: 'Optimistic' })}
+              ${NormalRow({ key: 'Data Validity:', value: 'Bitcoin' })}
+              ${NormalRow({ key: 'Data Availability:', value: 'Polygon' })}
               ${NormalRow({ key: 'Launch date:', value: 'June 27, 2023' })}
             </ul>
-          </div>
         `,
         banner: 'banner-04.png',
         link: GAME_LINK.ALPHA,
@@ -219,18 +213,16 @@ const ActivitiesVer2 = React.memo(() => {
       {
         title: 'AI on Bitcoin',
         desc: `
-          <div style='display: flex; flex-direction: ${isMobile ? "column" : "row"};justify-content: space-between; color: black'>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
+            <ul style='color: white'>
               ${LinkRow({ key: 'Bitcoin L2:', value: 'Eternal AI', link: GAME_LINK.AI })}
               ${NormalRow({ key: 'Block time:', value: '2 seconds' })}
-              ${NormalRow({ key: 'Data availability layer:', value: 'Bitcoin + Avail' })}
-            </ul>
-            <ul style='width: ${isMobile ? "100%" : "50%"}'>
               ${NormalRow({ key: 'Network type:', value: 'Bitcoin Mainnet' })}
               ${NormalRow({ key: 'Deployer:', value: 'NBC team' })}
+              ${NormalRow({ key: 'Rollups:', value: 'Optimistic' })}
+              ${NormalRow({ key: 'Data Validity:', value: 'Bitcoin' })}
+              ${NormalRow({ key: 'Data Availability:', value: 'Avail' })}
               ${NormalRow({ key: 'Launch date:', value: 'January 01, 2024' })}
             </ul>
-          </div>
         `,
         subTitle: '',
         banner: 'banner-05.png',
@@ -240,12 +232,9 @@ const ActivitiesVer2 = React.memo(() => {
       {
         title: 'Education on Bitcoin',
         desc: `
-          <ul style='color: black'>
-            ${NormalRow({
-          key: 'Activities:',
-          value: 'Learn about modular blockchain architecture via a fun Lego game.',
-        })}
+          <ul style='color: white'>
             ${LinkRow({ key: 'Bitcoin L2:', value: 'Modular', link: GAME_LINK.MODULAR })}
+            ${NormalRow({ key: 'Activities:', value: 'Learn about modular blockchain architecture via a fun Lego game.', })}
           </ul>
         `,
         banner: 'banner-03.png',
@@ -358,7 +347,7 @@ const ActivitiesVer2 = React.memo(() => {
           component1
         )}
         {!!component2 && (
-          <Box w="1px" height="9px" bg="black" opacity={0.7}/>
+          <Box w="1px" height="9px" bg="white" opacity={0.7}/>
         )}
         {!!component2 && (
           component2
@@ -369,17 +358,97 @@ const ActivitiesVer2 = React.memo(() => {
 
   const renderItem = (item: GameItemProps) => {
     return (
+      <Box
+        className={cx(s.container_item /*[ActivityType.AI].includes(item?.type) ? styles.special : ''*/)}
+        key={item.title}
+      >
+        <SimpleGrid
+          columns={[1, 1, 2]}
+          className={cx(s.container_item_inner, s[ActivityKey[item?.type]] /*[ActivityType.AI].includes(item?.type) ? styles.special : ''*/)}
+        >
+          <GridItem>
+            <Box className={s.container_item_media}>
+              {!!item.banner && (
+                (!!item.link || !!item.bannerLink) ?
+                  (
+                    <a href={item.bannerLink || item.link || ''} target='_blank'>
+                      <Image draggable={false} src={`public-sale/${item.banner}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='banner' />
+                    </a>
+                  ) :
+                  (
+                    <Image draggable={false} src={`public-sale/${item.banner}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='banner' />
+                  )
+              )}
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box p={"8px 8px"}>
+              <div className={s.container_item_header}>
+                <Text color='white' fontSize='20px' fontWeight='500'>
+                  {item.title}
+                  {!!item.subTitle && <span style={{ fontWeight: '400' }}>{item.subTitle}</span>}
+                </Text>
+              </div>
+              <div className={s.container_item_content}>
+                <div dangerouslySetInnerHTML={{ __html: item.desc }} />
+                {
+                  [ActivityType.AI].includes(item?.type) && (
+                    <div>
+                      <Tooltip
+                        minW="220px"
+                        bg="#007659"
+                        isOpen={isOpenFDV}
+                        // boxShadow="rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;"
+                        borderRadius="4px"
+                        padding="16px"
+                        label={
+                          <Flex direction="column" color="white" gap={"4px"}>
+                            <Text>- Receive 1,000 testnet $EAI for each AI model created.</Text>
+                            <Text>- Token Airdrop for the Top 3 most-used models - To Be Announced.</Text>
+                            <Text>- Token Airdrop for the Top 3 AI models with the most likes on X.</Text>
+                            <Text>- Token Airdrop for the Top 3 users who use the AI models the most.</Text>
+                            <Text>- Token Airdrop for the Top 3 users who use the AI models the most.</Text>
+                            <Text>- Token Airdrop also extends to $BVM holders, $GM holders, Perceptrons holders, Alpha OGs, NakaChain OGs, Bitcoin Arcade OGs, and users of other Bitcoin L2 platforms powered by BVM.</Text>
+                          </Flex>
+                        }
+                        className={s.aiPrizeInfo}
+                      >
+                        <Flex alignItems="center"
+                              onClick={onToggleFDV}
+                              onMouseEnter={onOpenFDV}
+                              onMouseLeave={onCloseFDV}
+                              className={s.aiPrizeIcon}
+                        >
+                          <svg width="14px" height="14px" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.66667 0.333984C2.98667 0.333984 0 3.32065 0 7.00065C0 10.6807 2.98667 13.6673 6.66667 13.6673C10.3467 13.6673 13.3333 10.6807 13.3333 7.00065C13.3333 3.32065 10.3467 0.333984 6.66667 0.333984ZM7.33333 10.334H6V6.33398H7.33333V10.334ZM7.33333 5.00065H6V3.66732H7.33333V5.00065Z" fill="#FFFFFF"/>
+                          </svg>
+                        </Flex>
+                      </Tooltip>
+                    </div>
+                  )
+                }
+              </div>
+            </Box>
+            <div className={s.container_item_footer}>
+              {renderReport(item.type)}
+            </div>
+          </GridItem>
+        </SimpleGrid>
+      </Box>
+
+    );
+    return (
       <Flex flexDir='column'
-            className={cx(styles.container_item, /*[ActivityType.AI].includes(item?.type) ? styles.special : ''*/)}
+            className={cx(s.container_item, /*[ActivityType.AI].includes(item?.type) ? styles.special : ''*/)}
             key={item.title}>
-        <div className={styles.container_item_header}>
-          <Text color='black' fontSize='20px' fontWeight='500'>
+        <div className={s.container_item_header}>
+          <Text color='white' fontSize='20px' fontWeight='500'>
             {item.title}
             {!!item.subTitle && <span style={{ fontWeight: '400' }}>{item.subTitle}</span>}
           </Text>
           {renderReport(item.type)}
         </div>
-        <div className={styles.container_item_content}>
+        <div className={s.container_item_content}>
           <div dangerouslySetInnerHTML={{ __html: item.desc }} />
           {
             [ActivityType.AI].includes(item?.type) && (
@@ -401,13 +470,13 @@ const ActivitiesVer2 = React.memo(() => {
                       <Text>- Token Airdrop also extends to $BVM holders, $GM holders, Perceptrons holders, Alpha OGs, NakaChain OGs, Bitcoin Arcade OGs, and users of other Bitcoin L2 platforms powered by BVM.</Text>
                     </Flex>
                   }
-                  className={styles.aiPrizeInfo}
+                  className={s.aiPrizeInfo}
                 >
                   <Flex alignItems="center"
                         onClick={onToggleFDV}
                         onMouseEnter={onOpenFDV}
                         onMouseLeave={onCloseFDV}
-                        className={styles.aiPrizeIcon}
+                        className={s.aiPrizeIcon}
                   >
                     <svg width="14px" height="14px" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M6.66667 0.333984C2.98667 0.333984 0 3.32065 0 7.00065C0 10.6807 2.98667 13.6673 6.66667 13.6673C10.3467 13.6673 13.3333 10.6807 13.3333 7.00065C13.3333 3.32065 10.3467 0.333984 6.66667 0.333984ZM7.33333 10.334H6V6.33398H7.33333V10.334ZM7.33333 5.00065H6V3.66732H7.33333V5.00065Z" fill="#FFFFFF"/>
@@ -418,7 +487,7 @@ const ActivitiesVer2 = React.memo(() => {
             )
           }
         </div>
-        <div className={styles.container_item_media}>
+        <div className={s.container_item_media}>
           {!!item.banner && (
             (!!item.link || !!item.bannerLink) ?
               (
@@ -436,8 +505,8 @@ const ActivitiesVer2 = React.memo(() => {
   };
 
   return (
-    <Box className={styles.wrap}>
-      <Flex flexDir='column' gap='20px' className={styles.container}>
+    <Box className={s.wrap}>
+      <Flex flexDir='column' gap='20px' className={s.container}>
         <SimpleGrid columns={{ base: 1, md: 1 }} gap='20px'>
           {TASKS.map(renderItem)}
         </SimpleGrid>
