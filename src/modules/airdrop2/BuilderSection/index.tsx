3588c38f3b1f1d11c424b93fc77ca62881fe86a8
richter <richter@newbitcoincity.com>
2024-02-29 15:37:38 +0700

    fix: add extra text

--- src/modules/airdrop2/BuilderSection/index.tsx
+++ src/modules/airdrop2/BuilderSection/index.tsx
@@ -25,6 +25,7 @@ const BUILDER_POINTS = [
     id: 0,
     title: 'Your Bitcoin L2 testnet goes live',
     point: '+1,000 PTS',
+    extraText: null,
     description:
       'Simply customize and launch your own Bitcoin L2 block for free in just a few clicks.',
     renderFooter: (
@@ -37,6 +38,7 @@ const BUILDER_POINTS = [
     id: 1,
     title: 'Deploy a smart contract on testnet',
     point: '+5,000 PTS',
+    extraText: null,
     description:
       'Since BVM is EVM equivalent, you can migrate your Solidity smart contracts and dapps from Ethereum (or other chains) to your Bitcoin L2 with minimal or no modification.',
     renderFooter: <button className={s.activeBtn}>Developer guides</button>,
@@ -45,6 +47,7 @@ const BUILDER_POINTS = [
     id: 2,
     title: 'Number of active wallets on testnet',
     point: '+1 PTS',
+    extraText: 'per active wallet',
     description:
       'The more active wallets in your Bitcoin L2 blockchain, the more Builder Points you get.',
     renderFooter: <button className={s.activeBtn}>Developer guides</button>,
@@ -85,6 +88,7 @@ const BUILDER_POINTS = [
     id: 4,
     title: 'TVL of your Bitcoin L2 mainnet',
     point: '+10 PTS',
+    extraText: 'per 1$ in TVL',
     description:
       'The higher the TVL in your Bitcoin L2 blockchain, the more Builder Points you get.',
     renderFooter: null,
@@ -104,7 +108,6 @@ const BuilderSection = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(111, listBuilders);
 
   return (
     <Flex
@@ -160,13 +163,14 @@ const BuilderSection = () => {
             <Flex direction="column" gap="20px">
               {BUILDER_POINTS.map((item) => (
                 <Box key={item.id} className={s.itemPoint}>
-                  <Flex>
+                  <Flex gap="24px">
                     <Box>
                       <Text>{item.title}</Text>
                       <Text>{item.description}</Text>
                     </Box>
                     <Box>
                       <Text>{item.point}</Text>
+                      <Text>{item.extraText}</Text>
                     </Box>
                   </Flex>
                   {item.renderFooter}
