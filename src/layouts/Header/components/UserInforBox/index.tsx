79ba10c33527678fb7c314ba1cb1a7109808f17f
tonytc13579 <tony@trustless.computer>
2024-02-29 14:10:11 +0700

    modify code

--- src/layouts/Header/components/UserInforBox/index.tsx
+++ src/layouts/Header/components/UserInforBox/index.tsx
@@ -1,24 +1,65 @@
 import { useAuthenticatedUserInfo } from '@/Providers/AuthenticatedProvider/hooks';
 import Avatar from '@/components/Avatar';
 import { Flex, Text } from '@chakra-ui/react';
-import { usePathname } from 'next/navigation';
-import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 const UserInforBox: React.FC = (): React.ReactElement => {
   const userInfor = useAuthenticatedUserInfo();
+  const router = useRouter();
   const pathname = usePathname();
+  const [isShow, setShow] = useState(true);
+
+  useEffect(() => {
+    if (pathname === '/') {
+      setShow(false);
+      setTimeout(() => {
+        setShow(true);
+      }, 1000);
+    } else {
+      setShow(true);
+    }
+  }, [pathname]);
+
+  const getBgColor = () => {
+    if (pathname === '/') {
+      return 'transparent';
+    }
+    if (pathname?.includes('blockchains')) {
+      return '#F3F1E8';
+    }
+    return '#F6F6F6';
+  };
+
+  const getBorderColor = () => {
+    if (pathname === '/') {
+      return 'white';
+    }
+    if (pathname?.includes('blockchains')) {
+      return '#d5d5d5';
+    }
+    return '#ECECEC';
+  };
+
+  if (!isShow) return <></>;
   return (
     <Flex
       flexDir={'row'}
       align={'center'}
       gap="5px"
-      bgColor={pathname === '/' ? '#2E2E2E' : '#F6F6F6'}
+      bgColor={getBgColor()}
       px={'5px'}
       py={'3px'}
       borderRadius={'100px'}
-      borderColor={pathname === '/' ? '#898989' : '#ECECEC'}
-      border={'1px'}
+      borderColor={getBorderColor()}
+      borderWidth={'1px'}
       maxW={'156px'}
+      _hover={{
+        cursor: 'pointer',
+      }}
+      onClick={() => {
+        router.push('/blockchains');
+      }}
     >
       <Avatar url={userInfor?.profile_image || ''} width={36} />
       <Flex flexDir={'row'} align={'center'} gap={'2px'} overflow={'hidden'}>
