078137953b0c80be47ecdf5537c9eb16a63e7f95
black1714 <black@newbitcoincity.com>
2024-03-12 14:01:35 +0700

    test metadata

--- src/app/smart-contracts-on-bitcoin/page.tsx
+++ src/app/smart-contracts-on-bitcoin/page.tsx
@@ -1,12 +1,26 @@
 'use client';
 
+import { CDN_URL } from '@/config';
 import MainLayout from '@/layouts/MainLayout';
 import BvmSctLanding from '@/modules/bvm-sct-landing';
+import Head from 'next/head';
 
 export default function Builder() {
   return (
-    <MainLayout>
-      <BvmSctLanding />
-    </MainLayout>
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content={`${CDN_URL}/images/smart-contract-on-bitcoin.png`}
+        />
+        <meta
+          name="twitter:image"
+          content={`${CDN_URL}/images/smart-contract-on-bitcoin.png`}
+        />
+      </Head>
+      <MainLayout>
+        <BvmSctLanding />
+      </MainLayout>
+    </>
   );
 }
