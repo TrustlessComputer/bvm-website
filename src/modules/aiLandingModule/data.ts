0fd2c0e7d06f9af3363f3445c5cea3cc9aff8d2a
luin <1103@generative.xyz>
2024-03-19 15:28:24 +0700

    update: hero

---
+++ src/modules/aiLandingModule/data.ts
@@ -0,0 +1,5 @@
+export const HeroData = {
+  title: 'Decentralized AI Module',
+  desc: 'Provide the requisite infrastructure and tools essential for defining, training, deploying neural network models and conducting effective on-chain inference with these models.',
+  link: '#',
+}
