53a8cd6f11649902c5dae49455c991ee2a1cba7d
Max <5008@generative.xyz>
2024-03-19 15:56:46 +0700

    update

--- src/modules/aiLandingModule/data.ts
+++ src/modules/aiLandingModule/data.ts
@@ -1,5 +1,3 @@
-import ModuleArchi from '@/modules/aiLandingModule/section/ModuleArchi';
-
 export const HeroData = {
   title: 'Decentralized AI Module',
   desc: 'Provide the requisite infrastructure and tools essential for defining, training, deploying neural network models and conducting effective on-chain inference with these models.',
