fee54b6938b89bf5e7f3d1d501551d1b28b9681a
luin <1103@generative.xyz>
2024-03-19 15:38:07 +0700

    update: header

--- src/app/ai-module/page.tsx
+++ src/app/ai-module/page.tsx
@@ -1,7 +1,16 @@
+import MainLayout from '@/layouts/MainLayout';
 import AiLandingModule from '@/modules/aiLandingModule';
 
 const AiModule = () => {
-  return <AiLandingModule/>
-}
+  return (
+    <MainLayout
+      headerProps={{
+        color: 'white',
+      }}
+    >
+      <AiLandingModule />
+    </MainLayout>
+  );
+};
 
 export default AiModule;
