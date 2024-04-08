841dc181a082f9d5741386839f3b4754494d9013
Max <5008@generative.xyz>
2024-04-08 11:04:09 +0700

    update header

---
+++ src/layouts/HeaderV2/Top/index.tsx
@@ -0,0 +1,21 @@
+import s from './style.module.scss'
+
+
+const Top = () => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.left}>
+        <div className={s.treasury}>
+          <p>Treasury: <span>$123M</span></p>
+        </div>
+        <p className={s.leftToken}>DAO</p>
+      </div>
+      <div className={s.right}>
+        <p className={s.token__orange}>$BVM</p>
+        <p className={s.token__green}>$SHARD</p>
+      </div>
+    </div>
+  )
+}
+
+export default Top;
