ce83cdc3c59b99f4dbe9497cf2aea23cb618cfbc
Venn22 <2411@generative.xyz>
2024-04-09 11:15:34 +0700

    update arrow hero

--- src/modules/landingV2/Componets/Hero/index.tsx
+++ src/modules/landingV2/Componets/Hero/index.tsx
@@ -11,47 +11,71 @@ import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import SlideItem from '@/modules/landingV2/Componets/Hero/SlideItem';
 import s from './styles.module.scss';
 import Container from '@/modules/landingV2/Componets/Container';
+import SvgInset from '@/components/SvgInset';
 
 export default function HeroV2() {
+  return (
+    <div className={s.hero}>
+      <Container>
+        <Splide
+          hasTrack={false}
+          options={{
+            type: 'loop',
+            focus: 'center',
+            autoWidth: true,
+            updateOnMove: true,
+          }}
+        >
+          <SplideTrack>
+            <SplideSlide>
+              <SlideItem
+                title={'Eternal AI'}
+                src={'/images/hero-slide-item.jpeg'}
+                action={'#'}
+              >
+                Powerful infrastructure and tools to build and scale your own
+                Bitcoin L2 with ease.
+              </SlideItem>
+            </SplideSlide>
+            <SplideSlide>
+              <SlideItem
+                title={'Eternal AI'}
+                src={'/images/hero-slide-item.jpeg'}
+                action={'#'}
+              >
+                Powerful infrastructure and tools to build and scale your own
+                Bitcoin L2 with ease.
+              </SlideItem>
+            </SplideSlide>
+            <SplideSlide>
+              <SlideItem
+                title={'Eternal AI'}
+                src={'/images/hero-slide-item.jpeg'}
+                action={'#'}
+              >
+                Powerful infrastructure and tools to build and scale your own
+                Bitcoin L2 with ease.
+              </SlideItem>
+            </SplideSlide>
+          </SplideTrack>
 
-  return <div className={s.hero}>
-    <Container>
-      <Splide hasTrack={false} options={{
-        type: 'loop',
-        focus: 'center',
-        autoWidth: true,
-        updateOnMove: true,
-      }}>
-        <SplideTrack>
-          <SplideSlide>
-            <SlideItem title={'Eternal AI'} src={'/images/hero-slide-item.jpeg'} action={'#'}>Powerful infrastructure
-              and
-              tools to build and scale your own Bitcoin L2 with ease.</SlideItem>
-          </SplideSlide>
-          <SplideSlide>
-            <SlideItem title={'Eternal AI'} src={'/images/hero-slide-item.jpeg'} action={'#'}>Powerful infrastructure
-              and
-              tools to build and scale your own Bitcoin L2 with ease.</SlideItem>
-          </SplideSlide>
-          <SplideSlide>
-            <SlideItem title={'Eternal AI'} src={'/images/hero-slide-item.jpeg'} action={'#'}>Powerful infrastructure
-              and
-              tools to build and scale your own Bitcoin L2 with ease.</SlideItem>
-          </SplideSlide>
-        </SplideTrack>
-
-        <div className='splide__arrows'>
-          <button className='splide__arrow splide__arrow--prev'>
-            <span>
-              <img src={'/landing-v2/arrow-right.svg'} alt={'arrow-right'} className={'reverse'} />
-            </span>
-          </button>
-          <button className='splide__arrow splide__arrow--next'>
-          <span><img src={'/landing-v2/arrow-right.svg'}
-                     alt={'arrow-right'} /></span>
-          </button>
-        </div>
-      </Splide>
-    </Container>
-  </div>;
+          <div className="splide__arrows">
+            <button className="splide__arrow splide__arrow--prev">
+              <span>
+                <SvgInset
+                  svgUrl={'/landing-v2/svg/arrow-l.svg'}
+                  className={'reverse'}
+                />
+              </span>
+            </button>
+            <button className="splide__arrow splide__arrow--next">
+              <span>
+                <SvgInset svgUrl={'/landing-v2/svg/arrow-r.svg'} />
+              </span>
+            </button>
+          </div>
+        </Splide>
+      </Container>
+    </div>
+  );
 }
