a21f0d130c57d9438fea6e732acd446d26a22811
Venn22 <2411@generative.xyz>
2024-04-15 15:51:11 +0700

    update arrow btn home

--- src/modules/landingV2/Componets/Section_7/index.tsx
+++ src/modules/landingV2/Componets/Section_7/index.tsx
@@ -15,6 +15,7 @@ import { useMemo } from 'react';
 import SectionTitle from '../SectionTitle';
 import { BLOGS, IBlog, LOGOS } from './constant';
 import { useIsMobile, useIsTablet } from '@/hooks/useWindowResize';
+import SvgInset from '@/components/SvgInset';
 
 const Section_7 = () => {
   const isMobile = useIsMobile();
@@ -41,7 +42,7 @@ const Section_7 = () => {
               <div className={s.cardLogo}>
                 <Image
                   src={getLogo}
-                  alt='thumb image'
+                  alt="thumb image"
                   width={'100%'}
                   height={28}
                   objectFit={'contain'}
@@ -53,7 +54,7 @@ const Section_7 = () => {
             <Image
               src={item.imageUrl}
               className={s.cardLogo_img}
-              alt='thumb image'
+              alt="thumb image"
               width={'100%'}
               height={186}
               objectFit={'cover'}
@@ -73,8 +74,8 @@ const Section_7 = () => {
 
   return (
     <div className={s.wrapper}>
-      <div className='container'>
-        <SectionTitle className={s.wrapper_title} textAlign='left'>
+      <div className="container">
+        <SectionTitle className={s.wrapper_title} textAlign="left">
           Oh, and the press loves us too!
         </SectionTitle>
         <div className={s.sliderContainer}>
@@ -86,8 +87,20 @@ const Section_7 = () => {
             slidesToShow={numberSlide}
             slidesToScroll={numberSlide}
             autoplay={false}
-            nextArrow={<br />}
-            prevArrow={<br />}
+            prevArrow={
+              <SvgInset
+                className={s.btn}
+                svgUrl={'/landing-v2/svg/left_circle.svg'}
+                size={48}
+              />
+            }
+            nextArrow={
+              <SvgInset
+                className={s.btn}
+                svgUrl={'/landing-v2/svg/right_circle.svg'}
+                size={48}
+              />
+            }
           >
             {BLOGS.map(renderCard)}
           </SliderSlick>
