40259e99c882938c87a8b2228cd18d512fd1288a
sett <dr.cat911@generative.xyz>
2024-05-06 17:06:08 +0700

    git stash

--- src/components/Retro/RetroCard/index.tsx
+++ src/components/Retro/RetroCard/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import s from './styles.module.scss'
 import ImagePlaceholder from '@components/ImagePlaceholder';
 
+export interface RetroCardProps extends PropsWithChildren{
+  src: string,
+  title: string
+}
 
-
-function Card(): React.JSX.Element {
+function Card({children, title, src}: RetroCardProps): React.JSX.Element {
   return (
     <div className={s.wrapper}>
       <div className={s.thumbnail}>
-        <ImagePlaceholder src={'/retro/1.png'} alt={'thumbnail'} width={1003} height={1002}/>
+        <ImagePlaceholder src={src} alt={'thumbnail'} width={1003} height={1002} />
       </div>
       <div className={s.content}>
-        <p className={s.title}>Unlock unparalleled performance</p>
-        <p className={s.desc}>Experience lightning-fast 2-second block times and ultra-low gas fees (less than $0.001 per transaction).</p>
+        <h2 className={s.title}>{title}</h2>
+        <div className={s.desc}>{children}</div>
       </div>
     </div>
   )
--- src/modules/gamefi/index.tsx
+++ src/modules/gamefi/index.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import { Box } from '@chakra-ui/react';
-import CategorySection from '@/modules/gamefi/CategorySection';
-import FooterSection from '@/modules/gamefi/FooterSection';
-import HeroSection from '@/modules/gamefi/HeroSection';
 import s from './styles.module.scss';
 import RetroHero from '@components/Retro/RetroHero';
+import ListInfo from '@/modules/gamefi/ListInfo';
+import RetroCaseStudy from '@components/Retro/RetroCaseStudy';
 
 const GameFiModule = () => {
   return (
-    <Box className={s.container} bgColor={'#f6f6f6'}>
+    <div className={s.gamefi}>
       <RetroHero label={'Designed for Game builders'} btn1={{
         title: 'Create your own GameFi L2',
         link: '#',
@@ -23,7 +21,9 @@ const GameFiModule = () => {
       >
         Shape the Future of Gaming on Bitcoin
       </RetroHero>
-    </Box>
+      <ListInfo />
+      <RetroCaseStudy />
+    </div>
   );
 };
 
