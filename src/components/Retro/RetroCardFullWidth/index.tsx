59751a27a3700dc07df827b63efa79a89ce8a93e
sett <dr.cat911@generative.xyz>
2024-05-07 13:32:02 +0700

    reboot page custommer

---
+++ src/components/Retro/RetroCardFullWidth/index.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import s from './styles.module.scss';
+import MetaItem from '@components/Retro/RetroCardFullWidth/MetaItem';
+
+export interface RetroCardFullWidthProps {
+  width?: number;
+  height?: number;
+  src: string,
+  heading: string,
+  metaData: {
+    label: string;
+    value: string;
+  }[]
+}
+
+export default function RetroCardFullWidth({ heading, width, src, height, metaData }: RetroCardFullWidthProps) {
+
+  return <div className={s.retroCardFullWidth}>
+    <div className={s.inner}>
+      <div className='thumbnail'>
+        <Image src={src} alt={'thumbnail'} width={width || 930} height={height || 529} />
+      </div>
+      <div className='info'>
+        <h2 className={s.heading}>{heading}</h2>
+        <div className='metaData'>
+          {metaData.map(item => {
+            return <MetaItem value={item.value} label={item.label} key={item.label} />;
+          })}
+        </div>
+      </div>
+    </div>
+  </div>;
+}
