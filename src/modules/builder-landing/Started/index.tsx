adf8bf19af14141f2348cab83b35b814d0049330
Venn22 <2411@generative.xyz>
2024-05-09 11:22:17 +0700

    fix -fpnt size

--- src/modules/builder-landing/Started/index.tsx
+++ src/modules/builder-landing/Started/index.tsx
@@ -11,9 +11,7 @@ export default function Started() {
       <div className={s.started} id={'read-doc'}>
         <div className={`container ${s.started_container}`}>
           <HeadingSection className={s.heading}>
-            <Chars delay={0.2}>
-              It’s easy to get started
-            </Chars>
+            <Chars delay={0.2}>It’s easy to get started</Chars>
           </HeadingSection>
           <ul className={s.extends}>
             <li className={s.exItem}>
@@ -30,14 +28,16 @@ export default function Started() {
             </li>
             <li className={s.exItem}>
               <Fade delay={0.2}>
-                <IConContent step={2}
-                             lock={true}
-                             content={`BVM allows Ethereum developers to migrate their Solidity smart contracts and dApps from
+                <IConContent
+                  step={2}
+                  lock={true}
+                  content={`BVM allows Ethereum developers to migrate their Solidity smart contracts and dApps from
 Ethereum to Bitcoin with minimal or no modifications.`}
-                             title={'Developer guides'}
-                             blank={true}
-                             link={'https://docs.bvm.network/bvm/'}
-                             icon={'builder/step-2.png'}>
+                  title={'Developer guides'}
+                  blank={true}
+                  link={'https://docs.bvm.network/bvm/'}
+                  icon={'builder/step-2.png'}
+                >
                   Deploy or Migrate your EVM Dapps effortlessly
                 </IConContent>
               </Fade>
@@ -45,20 +45,19 @@ Ethereum to Bitcoin with minimal or no modifications.`}
             <li className={s.exItem}>
               <Fade delay={0.3}>
                 <IConContent
-                  // title={'Submit Form'}
-                  // blank={true}
-                  // link={'https://forms.gle/eUbL7nHuTPA3HLRz8'}
-                  step={3} icon={'builder/step-3.png'}>
+                  title={'Submit Form'}
+                  blank={true}
+                  link={'https://forms.gle/eUbL7nHuTPA3HLRz8'}
+                  step={3}
+                  icon={'builder/step-3.png'}
+                >
                   Launch your Bitcoin L2 on mainnet
                 </IConContent>
               </Fade>
             </li>
             <li className={s.exItem}>
               <Fade delay={0.4}>
-                <IConContent
-                  step={4}
-                  icon={'builder/step-4.png'}
-                >
+                <IConContent step={4} icon={'builder/step-4.png'}>
                   Grow TVL and Earn $BVM airdrop
                 </IConContent>
               </Fade>
