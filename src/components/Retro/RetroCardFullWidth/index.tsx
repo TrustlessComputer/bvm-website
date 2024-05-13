57a139ba9aa84a5536347532d7ad13ac9f5f1b26
Max <5008@generative.xyz>
2024-05-13 12:16:23 +0700

    update visual content customer

--- src/components/Retro/RetroCardFullWidth/index.tsx
+++ src/components/Retro/RetroCardFullWidth/index.tsx
@@ -1,33 +1,71 @@
 import Image from 'next/image';
 import s from './styles.module.scss';
 import MetaItem from '@components/Retro/RetroCardFullWidth/MetaItem';
+import ImagePlaceholder from '@components/ImagePlaceholder';
+import Link from 'next/link';
 
 export interface RetroCardFullWidthProps {
   width?: number;
   height?: number;
   heading: string,
-  banner: string,
-  backgroundColor: string,
-  metaData: {
+  noteHeading: string,
+  desc: string,
+  banner: string | undefined,
+  videoSrc: string | undefined,
+
+  social: {
+    icon: string;
     label: string;
-    value: string;
+    link: string;
   }[]
 }
 
-export default function RetroCardFullWidth({ heading, width, banner, backgroundColor,  height, metaData }: RetroCardFullWidthProps) {
+export default function RetroCardFullWidth({ heading, noteHeading, videoSrc, width, banner,  height, desc, social }: RetroCardFullWidthProps) {
 
   return <div className={s.retroCardFullWidth}>
     <div className={s.inner}>
-      <div className={s.thumbnail} style={{backgroundColor}}>
-        <Image src={`/use-bitcoin/${banner}`} alt={'thumbnail'} width={width || 930} height={height || 529} className={s.image} />
+      <div className={s.thumbnail}>
+        {
+          videoSrc ? (
+            <video
+              src={videoSrc}
+              width={930}
+              height={529}
+              muted={true}
+              // poster={poster}
+              autoPlay
+              playsInline
+              loop
+              className={s.image}
+            />
+            ) : (
+            <Image src={`/use-bitcoin/${banner}`} alt={'thumbnail'} width={width || 930} height={height || 529} className={s.image} />
+          )
+        }
+
       </div>
       <div className={s.info}>
-        <h2 className={s.heading}>{heading}</h2>
-        <div className={s.metaData}>
-          {metaData.map(item => {
-            return <MetaItem value={item.value} label={item.label} key={item.label} />;
-          })}
+        <h2 className={s.heading}>{heading} <span>{noteHeading}</span></h2>
+        <p className={s.decs}>
+          {desc}
+        </p>
+        <div className={s.social}>
+          {
+            social?.map(item=> (
+              <Link href={item.link} className={s.socialItem} target={'_blank'}>
+                <ImagePlaceholder src={item.icon} alt={item.label} width={16} height={16}/>
+                <p>
+                  {item.label}
+                </p>
+              </Link>
+            ))
+          }
         </div>
+        {/*<div className={s.metaData}>*/}
+        {/*  {metaData.map(item => {*/}
+        {/*    return <MetaItem value={item.value} label={item.label} key={item.label} />;*/}
+        {/*  })}*/}
+        {/*</div>*/}
       </div>
     </div>
   </div>;
--- src/modules/UseBitcoin/TopContentV2/index.tsx
+++ src/modules/UseBitcoin/TopContentV2/index.tsx
@@ -6,10 +6,10 @@ function TopContentV2() {
   return (
     <div className={s.wrapper}>
       <div className={s.inner}>
-        <p className={s.label}>Bitcoin L2 for DeFi</p>
-        <p className={s.heading}>Experience Bitcoin like never before.</p>
-        <p className={s.description}>Hop from one Bitcoin L2 to another to play on-chain games, trade BRC-20 futures, run for charity, learn about modular architecture, and more.</p>
-        <Link href={''} className={s.link}>Launch your Bitcoin L2</Link>
+        <p className={s.label}>Welcome to the future of Bitcoin</p>
+        <p className={s.heading}>Experience Bitcoin like never before</p>
+        <p className={s.description}>BVM is the Bitcoin L2 factory that has all the best-of-breed blockchain modules for builders to easily set up their own powerful Bitcoin L2s, customized for any purpose, from DeFi, GameFi, RWA, DePIN to AI.</p>
+        <Link href={'/blockchains/customize'} className={s.link}>Launch your Bitcoin L2</Link>
       </div>
     </div>
   );
