9797ea792cc171461be7654253fa83ec3ef02086
camewell <matterhorn@trustless.computer>
2024-05-15 15:28:25 +0700

    chore: update code

--- src/hooks/useRequestNakaAccount.tsx
+++ src/hooks/useRequestNakaAccount.tsx
@@ -1,16 +1,34 @@
-import { useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
 import {
   INakaConnectContext,
   NakaConnectContext,
 } from '@/Providers/NakaConnectProvider';
-import { useAppSelector } from '@/stores/hooks';
+import { useAppDispatch, useAppSelector } from '@/stores/hooks';
 import { nakaAddressSelector } from '@/stores/states/user/selector';
+import { resetUser } from '@/stores/states/user/reducer';
+import { resetStake } from '@/stores/states/stakingV2/reducer';
 
 const useNakaAuthen = () => {
   const { requestAccount, loading, requestSignMessage } = useContext(
     NakaConnectContext,
   ) as INakaConnectContext;
   const nakaAddress = useAppSelector(nakaAddressSelector);
+
+  const dispatch = useAppDispatch();
+
+  const onLogout = React.useCallback(() => {
+    dispatch(resetUser())
+    dispatch(resetStake())
+    setTimeout(() => {
+      dispatch(resetUser())
+      dispatch(resetStake())
+    }, 200)
+
+    setTimeout(() => {
+      window.location.reload()
+    }, 300)
+  }, [])
+
   return {
     nakaAddress,
     isAuthen: !!nakaAddress,
@@ -18,6 +36,7 @@ const useNakaAuthen = () => {
     requestSignMessage,
     buttonText: nakaAddress ? '' : 'Connect',
     loading: loading === 'account',
+    onLogout,
   };
 };
 
--- src/utils/storage/naka.storage.ts
+++ src/utils/storage/naka.storage.ts
@@ -7,10 +7,14 @@ class NakaUserStorage {
   public static getWalletToken = () => global?.window?.localStorage.getItem(NAKA_WALLET_AUTHEN) || '';
   public static setWalletToken = (token: string) =>
     global?.window?.localStorage.setItem(NAKA_WALLET_AUTHEN, token || '');
+  public static removeWalletToken = () =>
+    global?.window?.localStorage.removeItem(NAKA_WALLET_AUTHEN);
 
   public static getUserAddress = () => global?.window?.localStorage.getItem(ADDRESS_STORAGE) || '';
   public static setUserAddress = (address: string) =>
     global?.window?.localStorage.setItem(ADDRESS_STORAGE, address || '');
+  public static removeUserAddress = () =>
+    global?.window?.localStorage.removeItem(ADDRESS_STORAGE);
 }
 
 export default NakaUserStorage;
