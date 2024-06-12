c08080902f6aaf40c4d24da5e7c9152d83d73e4b
0xd22f9c <dr.cat911@bvm.network>
2024-06-12 15:48:03 +0700

    hero

--- src/modules/landingV3/Componets/Experience/shader.ts
+++ src/modules/landingV3/Componets/Experience/shader.ts
@@ -97,5 +97,5 @@ varying vec2 vUv;
 vec4 getTexture( sampler2D tex ) { return  texture2D( tex , vUv ) ; }
 uniform sampler2D baseTexture; uniform sampler2D bloomTexture;  
 void main() {
- gl_FragColor = ( getTexture( baseTexture ) + vec4( 1.0 ) * getTexture(
+ gl_FragColor = ( getTexture( baseTexture ) + vec4( 0.015 ) * getTexture(
   bloomTexture ) ); }`;
