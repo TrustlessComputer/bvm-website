78d5d3b3956bc5b49c6f543c8d5618a2ae93e1bb
Venn22 <2411@generative.xyz>
2024-06-13 17:41:30 +0700

    update actions

--- src/modules/bvm_v3/index.tsx
+++ src/modules/bvm_v3/index.tsx
@@ -16,20 +16,9 @@ import Tokens from './Token';
 import Partners from '@/modules/bvm_v2/Partners';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import Actions from './Actions';
 
-const ACTIONS = [
-  {
-    href: '/bvm',
-    label: '$BVM',
-  },
-  {
-    href: '/shard',
-    label: '$SHARD',
-  },
-];
 const BVMModule = () => {
-  const pathname = usePathname();
-
   return (
     <div className={`${s.wrapper}  `}>
       {/*<div className={s.topHero}>*/}
@@ -37,20 +26,7 @@ const BVMModule = () => {
       {/*  <RetroHero />*/}
       {/*</div>*/}
       <div className={`${s.inner} containerV3`}>
-        <div className={s.actions}>
-          {ACTIONS.map((item) => {
-            const isActive = pathname === item.href;
-            return (
-              <Link href={item.href} key={item.href}>
-                <div
-                  className={`${s.actions_button} ${isActive ? s.active : ''}`}
-                >
-                  <h6>{item.label}</h6>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
+        <Actions />
 
         <Box className={s.container}>
           <TgeModule />
---
+++ src/modules/shardv2/topMiners/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import MainLayout from '@layouts/MainLayout';
+import s from './styles.module.scss';
+import { Box } from '@chakra-ui/react';
+import Loader from '@/modules/builder-landing/Loader';
+import React from 'react';
+import AboveTheFold from '@/modules/shard/topMiners/aboveTheFold';
+import Leaderboard from '@/modules/shard/topMiners/Leaderboard';
+
+const ShardTopMinersModule = () => {
+  return (
+    <MainLayout
+      headerProps={{
+        color: 'black',
+      }}
+    >
+      <Loader />
+      <Box className={s.container} bgColor={'#FFF'}>
+        <div className={s.heroSection}>
+          <AboveTheFold />
+        </div>
+        <div className={s.categorySection}>
+          <Leaderboard />
+        </div>
+      </Box>
+    </MainLayout>
+  )
+};
+
+export default ShardTopMinersModule;
