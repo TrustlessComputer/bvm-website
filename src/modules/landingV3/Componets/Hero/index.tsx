2ae7e9038439c48ea1271254996b00167b4d1be8
0xd22f9c <dr.cat911@bvm.network>
2024-06-17 18:14:47 +0700

    fix hero home v3

--- src/modules/landingV3/Componets/Hero/index.tsx
+++ src/modules/landingV3/Componets/Hero/index.tsx
@@ -9,13 +9,16 @@ export default function HeroV3() {
     <div className={s.hero}>
       <div className={s.inner}>
         <div className={s.content}>
-          <p className={s.title}>
-            Launch your <br /> own <span>ZK-POWered blockchain</span> on bitcoin
+          <h2 className={s.title}>
+            Be the first to launch a <span>ZK Rollup</span> on Bitcoin
+          </h2>
+          <p className={s.desc}>
+            Secured by advanced math and cryptography, ZK rollups provide unlimited scale for Bitcoin while retaining its security and decentralization. They are high-performance, verifiable, and modular. Oh, and did we mention they are EVM-equivalent, too?
           </p>
           <div className={s.btn} onClick={() => router.push('/pricing')}>
             Launch now
           </div>
-          <p className={s.label}>From $99 per chain/month</p>
+          <p className={s.label}>From $99 per month</p>
         </div>
       </div>
       <ScrollMore />
