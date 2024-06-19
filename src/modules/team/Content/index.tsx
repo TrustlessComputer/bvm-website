df8ad52c3427592f55012dfdef352d362fa92df1
Venn22 <2411@generative.xyz>
2024-06-19 17:46:54 +0700

    update team

--- src/modules/team/Content/index.tsx
+++ src/modules/team/Content/index.tsx
@@ -9,27 +9,45 @@ export default function Content() {
     <div className="containerV3">
       <div className={s.content}>
         <div className={s.content_group}>
-          <h4 className={s.content_group_title}>5 years.</h4>
+          <h4 className={s.content_group_title}>Our story</h4>
           <p className={s.content_group_decs}>
-            That’s a magic number—not too short and not too long. 5 years is the
-            perfect length of time to build something meaningful. It’s just the
-            right amount of time to tackle a big, ambitious problem and make an
-            impact.
+            BVM was born in January 2023 when a group of friends came together
+            one weekend to brainstorm ideas. Every time a new concept surfaced,
+            we asked ourselves one question: Where do we see this project in
+            five years?
           </p>
         </div>
         <div className={s.content_group}>
-          <h4 className={s.content_group_title}>
-            So what do we want to build?
-          </h4>
+          <h4 className={s.content_group_title}>Five years.</h4>
           <p className={s.content_group_decs}>
-            The common thread with our ideas is that we want to build something
-            on Bitcoin. This could be: generative art, on-chain AI,
-            decentralized financial infrastructure, or even autonomous worlds.
-            <br />
-            <br />
-            We’re seeking to generalize the use of Bitcoin by making it more
-            than just a cryptocurrency. We want art, AI, Metaverse, DeFi and
-            games all on Bitcoin.
+            That’s a magic number—not too short, not too long. Five years is the
+            perfect timeframe to build something meaningful. It’s just the right
+            amount of time to tackle a big, ambitious problem and make an
+            impact.
+            <br /> <br />
+            So, what did we want to build?
+            <br /> <br />
+            That day, we bounced around various ideas, from generative art and
+            on-chain AI to decentralized financial infrastructure and autonomous
+            worlds.
+            <br /> <br />
+            The common thread? We wanted to build on Bitcoin.
+            <br /> <br />
+            It could be anything, but it had to be on Bitcoin. We shared a
+            vision: to make Bitcoin more than a cryptocurrency. We envision art,
+            AI, the Metaverse, DeFi, and games—all on Bitcoin.
+            <br /> <br />
+            We wanted to create a radically new way to explore Bitcoin.
+            <br /> <br />
+            Bitcoin, reimagined.
+            <br /> <br />
+            This is Bitcoin redesigned—for the next generation of Bitcoin
+            builders who want to reshape its future.
+            <br /> <br />
+            We’ve made significant progress, but there’s still much more to do.
+            <br /> <br />
+            It’s time to build.
+            <br /> <br />
           </p>
         </div>
       </div>
