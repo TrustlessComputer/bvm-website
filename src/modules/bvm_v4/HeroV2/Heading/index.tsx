299de24722ca3e93d4169d385b46963be3fff91a
Max <5008@generative.xyz>
2024-06-19 16:27:52 +0700

    update page bvm

---
+++ src/modules/bvm_v4/HeroV2/Heading/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import s from './styles.module.scss';
+import { IconHeading } from '../components/IconSvg';
+import Fade from '@/interactive/Fade';
+import Lines from '@/interactive/Lines';
+import Image from 'next/image';
+
+export default function Heading() {
+  return (
+    <div className={s.wrap}>
+      <Fade delay={0.2}>
+        <div className={s.heading}>
+          <Image
+            src={'/bvm/bvm-v2.png'}
+            width={290}
+            height={131}
+            alt="heading"
+            sizes={'100vw'}
+            quality={100}
+          />
+        </div>
+      </Fade>
+      <p className={s.headingText}>
+        <Lines delay={0.3}>
+          Welcome to the future of Bitcoin
+        </Lines>
+      </p>
+      <p className={s.desc}>
+        <Lines delay={0.3}>
+          Ethereum’s heyday is over. Bitcoin is the new home for dapps.
+        </Lines>
+      </p>
+    </div>
+  );
+}
