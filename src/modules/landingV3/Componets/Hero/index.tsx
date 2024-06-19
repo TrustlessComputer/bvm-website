bc8d9d379d36eec9227636940d35ff27e08a599d
Venn22 <2411@generative.xyz>
2024-06-19 12:19:30 +0700

    update home page

--- src/modules/landingV3/Componets/Hero/index.tsx
+++ src/modules/landingV3/Componets/Hero/index.tsx
@@ -1,6 +1,7 @@
 import s from './styles.module.scss';
 import { useRouter } from 'next/navigation';
 import ScrollMore from '@components/ScrollMore';
+import SvgInset from '@/components/SvgInset';
 
 export default function HeroV3() {
   const router = useRouter();
@@ -9,25 +10,48 @@ export default function HeroV3() {
     <div className={s.hero}>
       <div className={s.inner}>
         <div className={s.content}>
-          <h2 className={s.title}>
-            BitZK
-          </h2>
-          <h3 className={s.subTitle}>
-            Launch a ZK Rollup on Bitcoin for $99/mo.
-          </h3>
-          <p className={s.desc}>
-            BitZK brought Zero Knowledge to Bitcoin, so you can easily launch and scale your own ZK Rollup on Bitcoin.
-          </p>
-          <div className={s.btn} onClick={() => router.push('/pricing')}>
-            Launch now
+          <div className={s.content_heading}>
+            <SvgInset
+              svgUrl="/landingV3/svg/logo_hero.svg"
+              height={80}
+              size={80}
+            />
+            <h2 className={s.title}>BitZK</h2>
           </div>
-          <button className={s.read}>
+          <h3 className={s.subTitle}>ZK Rollups on Bitcoin for $99/mo.</h3>
+          <p className={s.desc}>
+            BVM brought Zero Knowledge to Bitcoin, so you can easily launch and
+            scale your own ZK Rollup on Bitcoin.
+          </p>
+          <div className={s.groupBtn}>
+            <div
+              className={`${s.btn} ${s.btn__primary}`}
+              onClick={() => router.push('/pricing')}
+            >
+              Get started with BitZK
+            </div>{' '}
+            <div className={`${s.btn} ${s.btn__secondary}`}>
+              Connect with a BVM team member
+            </div>
+          </div>
+          {/* <button className={s.read}>
             Read Docs
-            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6.5 12L10.5 8L6.5 4" stroke="black" stroke-width="1.2" stroke-linecap="round"
-                    stroke-linejoin="round" />
+            <svg
+              width="17"
+              height="16"
+              viewBox="0 0 17 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.5 12L10.5 8L6.5 4"
+                stroke="black"
+                stroke-width="1.2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
             </svg>
-          </button>
+          </button> */}
         </div>
       </div>
       <ScrollMore />
