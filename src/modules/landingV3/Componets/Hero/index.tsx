eb0d2a9edec7c0fa22bc661126ad4969dfaab0bd
Venn22 <2411@generative.xyz>
2024-06-21 11:43:45 +0700

    update hero

--- src/modules/landingV3/Componets/Hero/index.tsx
+++ src/modules/landingV3/Componets/Hero/index.tsx
@@ -6,6 +6,8 @@ import { useRef } from 'react';
 import { useContactUs } from '@/Providers/ContactUsProvider/hook';
 
 import Image from 'next/image';
+import SvgInset from '@/components/SvgInset';
+import IcLogoText from '../IcLogoText';
 
 export default function HeroV3() {
   const router = useRouter();
@@ -13,34 +15,50 @@ export default function HeroV3() {
   const { showContactUsModal } = useContactUs();
   return (
     <div className={s.hero} ref={ref}>
-      <div className={s.inner}>
+      <div className={`${s.inner} containerV3`}>
         <div className={s.content}>
           <h2 className={s.title}>
-            <Image className={s.title_image} src={'/bvm/zkbit.png'} width={160} height={160} alt={'zkbit'} />
-            BitZK</h2>
-          <h3 className={s.subTitle}>Launch a ZK Rollup on Bitcoin for <strong>$99/mo.</strong>
+            <div className={s.title_logo}>
+              <IcLogoText />
+            </div>
+            BitZK
+          </h2>
+          <h3 className={s.subTitle}>
+            ZK Rollups on Bitcoin for <strong>$99/mo.</strong>
           </h3>
           <p className={s.desc}>
-            We brought Zero Knowledge to Bitcoin, so you can easily build your own ZK Rollup on Bitcoin.
+            BVM brought Zero Knowledge to Bitcoin, so you can easily launch and
+            scale your own ZK Rollup on Bitcoin.
           </p>
           <div className={s.groupBtn}>
             <div
               className={`${s.btn} ${s.btn__primary}`}
               onClick={() => router.push('/pricing')}
             >
-              Get started
-            </div>
-            {' '}
+              Get started with BitZK
+            </div>{' '}
             <div
               onClick={() => {
                 showContactUsModal();
               }}
-              className={`${s.btn} ${s.btn__secondary}`}>
-              Contact us
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M6 12L10 8L6 4" stroke="#FA4E0E" stroke-width="1.2" stroke-linecap="round"
-                      stroke-linejoin="round"></path>
-              </svg>
+              className={`${s.btn} ${s.btn__secondary}`}
+            >
+              Connect with a BVM team member
+              {/* <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 12L10 8L6 4"
+                  stroke="#FA4E0E"
+                  stroke-width="1.2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg> */}
             </div>
           </div>
         </div>
