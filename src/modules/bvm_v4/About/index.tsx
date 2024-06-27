65e5e67f5c65ff939f386ee8af93b32bec502016
Max <5008@generative.xyz>
2024-06-27 16:21:31 +0700

    update layout bvm

---
+++ src/modules/bvm_v4/About/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import s from './style.module.scss'
+
+
+const About = (): React.JSX.Element => {
+  return (
+    <div className={s.wrapper}>
+      <div className={`${s.container} container`}>
+        <div className={s.content}>
+          <h2 className={s.title}>About</h2>
+          <p className={s.desc}>
+            BVM is the scalable infrastructure to make Bitcoin as generalized as possible — usable for far more than just a currency. Powered by BVM, developers can now build all kinds of infrastructures, like Bitcoin L2s and Bitcoin L3s, and decentralized applications on Bitcoin — DeFi, GameFi, Payments, AI, and more.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
