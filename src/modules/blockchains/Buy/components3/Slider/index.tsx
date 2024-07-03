bf69d39a793967d75ca60068060f5955be769f0f
Max <5008@generative.xyz>
2024-07-03 17:31:29 +0700

    fix style

--- src/modules/blockchains/Buy/components3/Slider/index.tsx
+++ src/modules/blockchains/Buy/components3/Slider/index.tsx
@@ -2,7 +2,7 @@ import s from './styles.module.scss';
 import Image from 'next/image';
 import React, { useRef, useState } from 'react';
 import { useOnClickOutside } from '@hooks/useOnClickOutside';
-
+import { Slider as SliderReact, SliderFilledTrack, SliderThumb, SliderTrack } from '@chakra-ui/react';
 
 const Slider = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -11,6 +11,13 @@ const Slider = () => {
   useOnClickOutside(ref, () => setIsOpen(false))
   const getBackgroundSize = () => {
     return { backgroundSize: `${(value * 100) / 100}% 100%` }; };
+
+  const onChange = (value: number) => {
+    if (isNaN(value)) {
+      return;
+    }
+    setValue(value);
+  };
   return (
     <div className={s.dropdown} >
       <div className={s.dropdown_inner} onClick={() => setIsOpen(!isOpen)}>
@@ -30,7 +37,24 @@ const Slider = () => {
       >
         <div className={s.dropdown_wrap} ref={ref}>
           <p className={s.text}>{value}</p>
-          <input style={{accentColor: '#fff'}} type="range" value={value}  min="1" max="100" onInput={(e) => setValue(e.currentTarget.value)} />
+          {/*<input style={{accentColor: '#fff'}} type="range" value={value}  min="1" max="100" onInput={(e) => setValue(e.currentTarget.value)} />*/}
+          <SliderReact
+            // isDisabled={
+            //   !!packageParam && Number(packageParam) === PRICING_PACKGE.Hacker
+            // }
+            onChange={onChange}
+            value={value}
+            min={1} // 2 hours
+            max={100} // 24 hours
+            step={1}
+          >
+            <SliderTrack className={s.slider}>
+              <SliderFilledTrack bg={'#ffffff'} />
+            </SliderTrack>
+            <SliderThumb
+              boxSize={5}
+            ></SliderThumb>
+          </SliderReact>
         </div>
       </div>
     </div>
