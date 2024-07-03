857520a1d1914345429942421b5e268d6b4f1b1a
Max <5008@generative.xyz>
2024-07-03 09:51:09 +0700

    update

--- src/modules/blockchains/Buy/index_v3.tsx
+++ src/modules/blockchains/Buy/index_v3.tsx
@@ -1,14 +1,18 @@
 import s from './styles.module.scss'
+import { useRouter } from 'next/navigation';
+import BoxOption from '@/modules/blockchains/Buy/components3/BoxOption';
 
 const BuyPage = () => {
+  const router = useRouter();
+
   return (
     <div className={s.wrapper}>
       <div className={s.inner}>
         <div className={s.left}>
           <p className={s.heading}>Customize your Blockchain</p>
           <div className={s.left_box}>
-            list option
-            <div className={s.boxItem}></div>
+            <BoxOption active={true} label={'1. Network'}/>
+            <BoxOption label={'2. Data Availability'}/>
           </div>
         </div>
         <div className={s.right}>
@@ -16,7 +20,9 @@ const BuyPage = () => {
             <p className={s.heading}>Your tier</p>
             <div  className={s.right_top_box}>
               <p><span>Hacker</span> $99 per rollup/month</p>
-              <div className={s.right_top_box_btn}>
+              <div className={s.right_top_box_btn} onClick={() => {
+                router.push('/pricing');
+              }}>
                 <p>Switch</p>
               </div>
             </div>
