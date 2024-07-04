9d15caae73e722be66064e8f447aa8febde7328b
0xd22f9c <dr.cat911@bvm.network>
2024-07-04 14:16:47 +0700

    remove item solutions

--- src/layouts/HeaderV3/components/GroupSolutions/index.tsx
+++ src/layouts/HeaderV3/components/GroupSolutions/index.tsx
@@ -8,31 +8,31 @@ export default function GroupProducts(): ReactElement {
   return <div className={s.groupProduction}>
     <div className={s.inner}>
       <div className={s.left}>
-        <div className={s.group}>
-          <h4 className={s.title}>By Chain</h4>
-          <ul className={`${s.group_inner} ${s.isFlex}`}>
-            <li className={s.isDisabled}>
-              <Link href={'/module/zk-rollup'} className={s.group_item_icon}>
-                <Image width={48} height={48} sizes="100vw" quality="100" src="/bvm/brc-20bridge.png"
-                       alt="menu-icon-avail" />
-                <div className={s.group_item_icon_text}>
-                  <span className={s.group_item_icon_text_title}>Bitcoin</span>
-                  <span className={s.group_item_icon_text_bottom}>Roll up to Bitcoin as Ordinal Inscriptions</span>
-                </div>
-              </Link>
-            </li>
-            <li className={s.isDisabled}>
-              <Link href={'/module/pptimistic-rollups'} className={s.group_item_icon}>
-                <Image width={48} height={48} sizes="100vw" quality="100" src="/bvm/ethereumbridge.png"
-                       alt="menu-icon-op" />
-                <div className={s.group_item_icon_text}>
-                  <span className={s.group_item_icon_text_title}>Ethereum</span>
-                  <span className={s.group_item_icon_text_bottom}>Embed proofs in Taproot transactions</span>
-                </div>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {/*<div className={s.group}>*/}
+        {/*  <h4 className={s.title}>By Chain</h4>*/}
+        {/*  <ul className={`${s.group_inner} ${s.isFlex}`}>*/}
+        {/*    <li className={s.isDisabled}>*/}
+        {/*      <Link href={'/module/zk-rollup'} className={s.group_item_icon}>*/}
+        {/*        <Image width={48} height={48} sizes="100vw" quality="100" src="/bvm/brc-20bridge.png"*/}
+        {/*               alt="menu-icon-avail" />*/}
+        {/*        <div className={s.group_item_icon_text}>*/}
+        {/*          <span className={s.group_item_icon_text_title}>Bitcoin</span>*/}
+        {/*          <span className={s.group_item_icon_text_bottom}>Roll up to Bitcoin as Ordinal Inscriptions</span>*/}
+        {/*        </div>*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*    <li className={s.isDisabled}>*/}
+        {/*      <Link href={'/module/pptimistic-rollups'} className={s.group_item_icon}>*/}
+        {/*        <Image width={48} height={48} sizes="100vw" quality="100" src="/bvm/ethereumbridge.png"*/}
+        {/*               alt="menu-icon-op" />*/}
+        {/*        <div className={s.group_item_icon_text}>*/}
+        {/*          <span className={s.group_item_icon_text_title}>Ethereum</span>*/}
+        {/*          <span className={s.group_item_icon_text_bottom}>Embed proofs in Taproot transactions</span>*/}
+        {/*        </div>*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*  </ul>*/}
+        {/*</div>*/}
         <div className={s.group}>
           <h4 className={s.title}>By Business model</h4>
           <ul className={`${s.group_inner} ${s.isFlex}`}>
