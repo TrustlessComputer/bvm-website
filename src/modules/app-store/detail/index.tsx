11128214c6d8ef5a056cff88858d242dc96feafe
0xmegalodon <stephen@newbitcoincity.com>
2024-07-04 16:02:58 +0700

    add app store page

--- src/modules/app-store/detail/index.tsx
+++ src/modules/app-store/detail/index.tsx
@@ -1,10 +1,41 @@
-import { Box } from '@chakra-ui/react';
+"use client";
+
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
 import s from './styles.module.scss';
+import { useParams, useRouter } from 'next/navigation';
+import { useMemo } from 'react';
+import { DATA } from '@/modules/app-store/data';
+import { compareString } from '@utils/string';
 
 const AppDetailModule = () => {
+  const params = useParams();
+  const router = useRouter();
+
+  const data: IAppInfo = useMemo(() => {
+    return DATA.find(d => compareString(d.id, params?.id)) || {} as IAppInfo;
+  }, [params?.id]);
+
+  const status = useMemo(() => {
+    return 'not_installed';
+  }, []);
+
+  const handleBack = () => {
+    return router.back();
+  }
+
   return (
     <Box className={s.container}>
-      AppDetailModule
+      <Box className={"containerV3"}>
+        <Flex className={s.back} onClick={handleBack}>
+          <Text>Back</Text>
+        </Flex>
+        <Box className={s.content}>
+          <Image className={s.avatar} src={data?.image}/>
+          <Text className={s.status}>{status}</Text>
+          <Text className={s.title}>{data?.title}</Text>
+          <Text className={s.description}>{data?.description}</Text>
+        </Box>
+      </Box>
     </Box>
   )
 }
--- src/modules/app-store/item/index.tsx
+++ src/modules/app-store/item/index.tsx
@@ -2,6 +2,7 @@ import { Flex, Image, Text } from '@chakra-ui/react';
 import s from './styles.module.scss';
 import { useRouter } from 'next/navigation';
 import { APP_STORE } from '@constants/route-path';
+import { useMemo } from 'react';
 
 const AppItem = ({data}: {data: IAppInfo}) => {
   const router = useRouter();
@@ -10,9 +11,14 @@ const AppItem = ({data}: {data: IAppInfo}) => {
     router.push(`${APP_STORE}/${data?.id}`)
   }
 
+  const status = useMemo(() => {
+    return 'not_installed';
+  }, [])
+
   return (
     <Flex className={s.container} onClick={handleSelectApp}>
-      <Image className={s.avatar}  src={data?.image}/>
+      <Image className={s.avatar} src={data?.image}/>
+      <Text className={s.status}>{status}</Text>
       <Text className={s.title}>{data?.title}</Text>
       <Text className={s.description}>{data?.description}</Text>
     </Flex>
