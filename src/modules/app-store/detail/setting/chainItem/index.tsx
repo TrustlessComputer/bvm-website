d7bbf6215e95ef5146d01971bd07990dff158f2e
0xmegalodon <stephen@newbitcoincity.com>
2024-07-08 16:57:20 +0700

    refactor

--- src/modules/app-store/detail/setting/chainItem/index.tsx
+++ src/modules/app-store/detail/setting/chainItem/index.tsx
@@ -3,6 +3,7 @@ import { Avatar, Flex, MenuItem, Text } from '@chakra-ui/react';
 import s from './styles.module.scss';
 import { OrderItem } from '@/stores/states/l2services/types';
 import cx from 'clsx';
+import { PRICING_PACKGE, PRICING_PACKGE_MAP } from '@/modules/PricingV2/constants';
 
 const ChainItem: React.FC<any> = ({ data, onSelectChain, isButton}: {data: OrderItem, onSelectChain: any, isButton: boolean}) => {
   if (!data) {
@@ -13,7 +14,7 @@ const ChainItem: React.FC<any> = ({ data, onSelectChain, isButton}: {data: Order
     return (
       <MenuItem className={s.container} w={"98%"}>
         <Flex justifyContent={"space-between"} alignItems={"center"} w={"100%"}>
-          <Text className={s.titleSelect}>{data?.chainName} - Hacker</Text>
+          <Text className={s.titleSelect}>{data?.chainName} - {PRICING_PACKGE_MAP[data?.package as PRICING_PACKGE]}</Text>
           <Text className={cx(s.package, data?.isNeedTopup ? s.needTopup : '')}>{data?.isNeedTopup ? 'Waiting for payment' : 'Basic'}</Text>
         </Flex>
       </MenuItem>
@@ -28,7 +29,7 @@ const ChainItem: React.FC<any> = ({ data, onSelectChain, isButton}: {data: Order
           <Flex direction={"column"} gap={"8px"}>
             <Text className={s.title}>{data?.chainName}</Text>
             <Flex alignItems={"center"} gap={"12px"}>
-              <Text className={s.chain_package}>Hacker</Text>
+              <Text className={s.chain_package}>{PRICING_PACKGE_MAP[data?.package as PRICING_PACKGE]}</Text>
               <Text className={s.price}>${data?.packagePriceUSD} per rollup/month</Text>
             </Flex>
           </Flex>
