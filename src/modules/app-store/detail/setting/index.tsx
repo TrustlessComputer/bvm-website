fe7d373528d47e937d871bb743113e6a593ff034
0xmegalodon <stephen@newbitcoincity.com>
2024-07-08 09:48:39 +0700

    update setting view

--- src/modules/app-store/detail/setting/index.tsx
+++ src/modules/app-store/detail/setting/index.tsx
@@ -1,11 +1,16 @@
 import { useAppSelector } from '@/stores/hooks';
-import { myOrderListFilteredByNetwork } from '@/stores/states/l2services/selector';
-import { Box } from '@chakra-ui/react';
+import { getL2ServicesStateSelector, myOrderListFilteredByNetwork } from '@/stores/states/l2services/selector';
+import { Box, Flex } from '@chakra-ui/react';
 import useL2Service from '@hooks/useL2Service';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import Loading from '@components/Loading';
+import s from './styles.module.scss';
 
 const SettingView = () => {
   const { getMyOrderList } = useL2Service();
+  const { accountInforL2Service, isMyOrderListFetched } = useAppSelector(
+    getL2ServicesStateSelector,
+  );
 
   useEffect(() => {
     getMyOrderList();
@@ -13,10 +18,22 @@ const SettingView = () => {
 
   const myOrders = useAppSelector(myOrderListFilteredByNetwork);
   console.log('myOrders', myOrders);
+  console.log('accountInforL2Service', accountInforL2Service);
+  console.log('isMyOrderListFetched', isMyOrderListFetched);
+  console.log('======')
 
   return (
     <Box>
-      SettingView
+      {
+        isMyOrderListFetched ? (
+          <Box>SettingView</Box>
+        ) : (
+          <Flex className={s.loadingContainer}>
+            <Loading />
+          </Flex>
+        )
+      }
+
     </Box>
   );
 };
