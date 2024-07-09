6f6ed488a6c4e2669f7fabe9c2e283875e182098
0xd22f9c <dr.cat911@bvm.network>
2024-07-09 10:28:04 +0700

    fx

--- src/modules/tagBlogModule/index.tsx
+++ src/modules/tagBlogModule/index.tsx
@@ -1,6 +1,5 @@
-'use client'
+'use client';
 
-import ListBlog from '@/modules/blog/list';
 import React from 'react';
 import s from './styles.module.scss';
 import Card from '@/modules/blog/list/Card';
@@ -9,22 +8,19 @@ import Filter from '@/modules/blog/list/Filter';
 const TagBlogModule = (props: Posts) => {
   return (
     <div className={s.wrapper}>
-      {/*<ListBlog {...props} className={s.list} isHome={false}/>*/}
+      <Filter />
       <div className={'containerV3'}>
-        <Filter />
         <div className={s.inner}>
           {
             props.data.map(item => {
               return (
                 <Card {...item} key={item.slug} />
-              )
+              );
             })
           }
-
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 export default TagBlogModule;
