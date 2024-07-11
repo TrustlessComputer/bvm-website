04f2d41ee77aa7908e52996e1934d674b101db49
0xd22f9c <dr.cat911@bvm.network>
2024-07-11 15:12:02 +0700

    refactor api

--- src/services/customize-model.ts
+++ src/services/customize-model.ts
@@ -2,7 +2,7 @@ export async function getModelCategories(): Promise<IModelCategory[] | null> {
 
   try {
     const data = await fetch('https://l2aas-api.newbitcoincity.com/api/order/available-list-v3').then(res => res.json());
-    return data.result.categories;
+    return data.result;
   } catch (error) {
     console.error(error);
   }
