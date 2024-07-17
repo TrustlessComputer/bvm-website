51e60b289432dc70ae8d62721587e5f41171d6f4
Jackie <1121@generative.xyz>
2024-07-17 13:11:32 +0700

    test dynamic url

--- src/app/rollups/customizev2/[pictureUrl]/page.tsx
+++ src/app/rollups/customizev2/[pictureUrl]/page.tsx
@@ -1,88 +1,52 @@
 import MainLayout from '@/layouts/MainLayout';
 
 import Page from '@/modules/blockchains/customize/index_v4';
-
 import { Metadata } from 'next';
-
 import { APP_NAME } from '@/config/metadata';
-
 import { useSearchParams } from 'next/navigation';
 
 // export const metadata: Metadata = {
-
-//   openGraph: {
-
-//     images: [
-
-//       {
-
-//         url: `https://storage.googleapis.com/bvm-network/image/metadata%202.png`,
-
-//         width: 1200,
-
-//         height: 630,
-
-//         alt: APP_NAME,
-
-//       },
-
-//     ],
-
-//   },
-
-//   twitter: {
-
-//     images: [
-
-//       {
-
-//         url: `https://storage.googleapis.com/bvm-network/image/metadata%202.png`,
-
-//         alt: APP_NAME,
-
-//       },
-
-//     ],
-
-//   },
-
+//   openGraph: {
+//     images: [
+//       {
+//         url: `https://storage.googleapis.com/bvm-network/image/metadata%202.png`,
+//         width: 1200,
+//         height: 630,
+//         alt: APP_NAME,
+//       },
+//     ],
+//   },
+//   twitter: {
+//     images: [
+//       {
+//         url: `https://storage.googleapis.com/bvm-network/image/metadata%202.png`,
+//         alt: APP_NAME,
+//       },
+//     ],
+//   },
 // };
 
-export async function generateMetadata({ params }: any) {
-  const pictureUrl = params.pictureUrl;
-
-  console.log(
-    '🚀 -> file: page.tsx:32 -> generateMetadata -> pictureUrl ::',
-
-    pictureUrl,
-  );
+export async function generateMetadata({ params, searchParams }: any) {
+  const pictureUrl = searchParams.pictureUrl;
 
   return {
     openGraph: {
       type: 'website',
-
       siteName: APP_NAME,
-
       images: [
         {
           url: pictureUrl,
-
           width: 1200,
-
           height: 630,
-
           alt: APP_NAME,
         },
       ],
     },
-
     twitter: {
       card: 'summary_large_image',
-
       images: [
         {
           // url: pictureUrl,
-
           alt: APP_NAME,
         },
       ],
@@ -95,7 +59,6 @@ const CustomizePage = () => {
     <MainLayout
       headerProps={{
         color: 'black',
-
         bgColor: '#F3F1E8',
       }}
       hideHeader={true}
