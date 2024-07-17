8be5dccff67219e85b7c0dd6e3166bebd98ff150
Max <5008@generative.xyz>
2024-07-17 14:57:32 +0700

    update

--- src/modules/blockchains/Buy/Capture.tsx
+++ src/modules/blockchains/Buy/Capture.tsx
@@ -1,34 +1,34 @@
-import { Button } from '@chakra-ui/react';
-import html2canvas from "html2canvas";
-import Image from 'next/image';
+import html2canvas from 'html2canvas';
 import s from '@/modules/blockchains/Buy/styles_v5.module.scss';
 import ImagePlaceholder from '@components/ImagePlaceholder';
 
 const Capture = ({...props}) => {
 
+  const exportBase64 = async ( ) => {
+    const canvasDom = document.querySelector('#imageCapture')
+    const canvas = await html2canvas(canvasDom).then((res) => {
+      props.setIsCapture(false);
+      return res;
+    });
+    return canvas.toDataURL("image/png", 1.0)
+  }
+
   const exportAsImage = async () => {
     props.setIsCapture(true);
-    const canvasDom = document.querySelector('#imageCapture')
     setTimeout( async () => {
-      const canvas = await html2canvas(canvasDom).then((res) => {
-        props.setIsCapture(false);
-        return res;
-      });
-      const image = canvas.toDataURL("image/png", 1.0);
-      console.log('image', image)
-      return image
+      return exportBase64()
     },1500)
-    // downloadImage(image, imageFileName);
   };
 
 
   async function download() {
+    props.setIsCapture(true);
     const a = document.createElement('a');
-    const base64 = await exportAsImage().then((res) => {
-      a.href = res
-      a.download = "Image.png";
+    setTimeout( async () => {
+      a.href = await exportBase64()
+      a.download = `${new Date}.png`;
       a.click();
-    })
+    },1500)
   }
 
   return (
