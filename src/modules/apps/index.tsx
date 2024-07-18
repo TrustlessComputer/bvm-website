20cfd8bdad4cc10b746b04bb6da6c034a5671325
0xmegalodon <stephen@newbitcoincity.com>
2024-07-18 17:10:08 +0700

    add customize app

---
+++ src/modules/apps/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Flex } from '@chakra-ui/react';
+import s from './styles.module.scss';
+import { BuyProvider } from '@/modules/blockchains/providers/Buy.context';
+import BuyPage from './Buy';
+
+
+export default () => {
+  return (
+    <Flex
+      flex={1}
+      flexDir={'column'}
+      align={'center'}
+      // py={['10px', '20px', '30px', '50px']}
+      className={s.container}
+    >
+      <BuyProvider>
+        <Flex w={'100%'} px={['16px', '18px', '20px']}>
+          <BuyPage />
+        </Flex>
+      </BuyProvider>
+    </Flex>
+  );
+};
