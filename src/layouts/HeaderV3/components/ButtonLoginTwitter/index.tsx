d0fb977ef61c3cd1338f9acdab5c9e37479d69a3
tony <tony@bvm.network>
2024-07-19 16:11:08 +0700

    Load avatar user

--- src/layouts/HeaderV3/components/ButtonLoginTwitter/index.tsx
+++ src/layouts/HeaderV3/components/ButtonLoginTwitter/index.tsx
@@ -9,6 +9,7 @@ import { useAppSelector } from '@/stores/hooks';
 import { accountInforSelector } from '@/stores/states/l2services/selector';
 import { useRouter } from 'next/navigation';
 import useL2Service from '@/hooks/useL2Service';
+import { Image } from '@chakra-ui/react';
 
 type Props = {
   color?: 'black' | 'white';
@@ -17,7 +18,7 @@ type Props = {
 };
 const ButtonLoginTwitter = (props: Props) => {
   const router = useRouter();
-  const { loggedIn, login, logout } = useWeb3Auth();
+  const { loggedIn, login, logout, userInfo } = useWeb3Auth();
   const { getAccountInfor } = useL2Service();
   const accInfor = useAppSelector(accountInforSelector);
 
@@ -54,6 +55,15 @@ const ButtonLoginTwitter = (props: Props) => {
       onMouseEnter={() => setIsHover(true)}
     >
       <div className={s.inner}>
+        {loggedIn && (
+          <Image
+            src={userInfo?.profileImage || '/icons/default-user-avatar.svg'}
+            w="28px"
+            h={'28px'}
+            borderRadius={'100%'}
+            fit={'cover'}
+          ></Image>
+        )}
         <p className={s.text}>
           {/* {!loggedIn ? 'Connect' : `${accInfor?.addressFormatted || '--'}`}{' '} */}
           {!loggedIn
