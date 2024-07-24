878def02ca1048340449e35967ae935d4353010d
0xmegalodon <stephen@newbitcoincity.com>
2024-07-24 15:49:58 +0700

    fix change app

--- src/modules/blockchains/dapp/hooks/useSubmitForm.ts
+++ src/modules/blockchains/dapp/hooks/useSubmitForm.ts
@@ -288,8 +288,6 @@ const useSubmitForm = () => {
         unvestAmounts,
       } = body;
 
-      console.log('body', body);
-
       let iface = new ethers.utils.Interface(TOKENABI.abi);
 
       const calldata = iface.encodeDeploy([
