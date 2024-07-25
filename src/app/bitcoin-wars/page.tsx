8b9152bf7bb7acb14f12f3e8b4c50e0819e3fde9
richter <richter@bvm.network>
2024-07-25 15:20:45 +0700

    fix: update meta data twitter

--- src/app/bitcoin-wars/page.tsx
+++ src/app/bitcoin-wars/page.tsx
@@ -7,9 +7,16 @@ import MagaModule from '@/modules/maga';
 export const metadata: Metadata = {
   title: 'Bitcoin Wars',
   description:
-    'Prepare your strategy and engage in the battle that will shape the future of Bitcoin. Starting at the Bitcoin 2024 Conference in Nashville.',
+    'Gear up for an epic adventure and strategize your way to victory in Bitcoin Wars, an incredibly fun onchain game on Bitcoin.',
   openGraph: {
     images: ['/maga/crypto-war.svg'],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Bitcoin Wars',
+    description:
+      'Gear up for an epic adventure and strategize your way to victory in Bitcoin Wars, an incredibly fun onchain game on Bitcoin.',
+    images: '/maga/crypto-war.svg',
   },
 };
 
