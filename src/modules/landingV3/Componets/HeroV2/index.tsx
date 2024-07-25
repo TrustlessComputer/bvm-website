cf559a6a80c5032b2aaa885b63972c8d9445e932
0xd22f9c <dr.cat911@bvm.network>
2024-07-25 16:30:21 +0700

    fx

--- src/modules/landingV3/Componets/HeroV2/index.tsx
+++ src/modules/landingV3/Componets/HeroV2/index.tsx
@@ -61,7 +61,7 @@ export default function HeroV2() {
             <div className={s.imageHero_bg}>
               {/*<video src={'https://storage.googleapis.com/bvm-network/image/output_v5.mp4'} loop preload="auto" playsInline muted autoPlay width={16} height={9} />*/}
               <Image className={s.imagePreload}
-                     src={'https://storage.googleapis.com/bvm-network/image/Drag%20and%20Drop%20Banner.gif'}
+                     src={'https://storage.googleapis.com/bvm-network/image/Drag%20and%20Drop%20Banner%2003.gif'}
                      width={1566}
                      height={880}
                      alt={'video'} sizes={'100vw'} quality={100} />
