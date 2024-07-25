b816214a16c51e5f54d6fd0b855c97013fc6df7d
Max <5008@generative.xyz>
2024-07-25 18:32:34 +0700

    update video

--- src/modules/landingV3/Componets/CaseStudy/index.tsx
+++ src/modules/landingV3/Componets/CaseStudy/index.tsx
@@ -1,11 +1,34 @@
 import s from './styles.module.scss'
 import Link from 'next/link';
+import Image from 'next/image';
+import { VIDEO_BVM_STUDIO_HOW_IT_WORK } from '@constants/common';
+import React, { useState } from 'react';
 
 export default function CaseStudy() {
+  const [isOpenModalVideo, setIsOpenModalVideo] = useState<boolean>(false);
+
   return <div className={'containerV3'}>
     <div className={s.inner}>
-      <div className={s.thumbnail}>
-        <img src="/vs.jpg" alt="vs" />
+      {/*<div className={s.thumbnail}>*/}
+      {/*  <img src="/vs.jpg" alt="vs" />*/}
+      {/*</div>*/}
+      <div className={s.thumbnail} onClick={() => setIsOpenModalVideo(true)}>
+        {!isOpenModalVideo &&
+          <div className={s.thumbnail_bg}>
+            {/*<video src={'https://storage.googleapis.com/bvm-network/image/output_v5.mp4'} loop preload="auto" playsInline muted autoPlay width={16} height={9} />*/}
+            <Image className={s.imagePreload}
+                   src={'/vs.jpg'}
+                   width={1566}
+                   height={880}
+                   alt={'video'} sizes={'100vw'} quality={100} />
+            <div className={s.thumbnail_btn}>
+              <Image src={'/play.svg'} alt={'icons'} width={20} height={20} />
+            </div>
+          </div>}
+
+        <video src={'https://storage.googleapis.com/bvm-network/image/Bitcoin%20Wars%20Intro.mp4'} width={160}
+               height={90}
+               preload="auto" playsInline controls />
       </div>
       <div className={s.inner_right}>
         <p className={s.tag}>Case Study</p>
