838de8c03767e1553459c1c5bfb4ca86e9037c60
richter <richter@bvm.network>
2024-07-29 11:36:31 +0700

    fix: update seo title maga page

--- src/app/bitcoin-wars/page.tsx
+++ src/app/bitcoin-wars/page.tsx
@@ -4,18 +4,31 @@ import '@fontsource/dosis';
 import MainLayout from '@/layouts/MainLayout';
 import MagaModule from '@/modules/maga';
 
+const APP_TITLE =
+  'Bitcoin Wars | The first fully on-chain game built on a ZK Rollup on the Bitcoin network.';
+const APP_DESCRIPTION =
+  'Gear up for an epic adventure and strategize your way to victory in Bitcoin Wars, an incredibly fun onchain game on Bitcoin.';
+
 export const metadata: Metadata = {
-  title: 'Bitcoin Wars',
-  description:
-    'Gear up for an epic adventure and strategize your way to victory in Bitcoin Wars, an incredibly fun onchain game on Bitcoin.',
+  title: {
+    absolute: APP_TITLE,
+  },
+  description: APP_DESCRIPTION,
   openGraph: {
+    title: {
+      absolute: APP_TITLE,
+    },
     images: ['/maga/crypto-war-seo.png'],
+  },
+  appleWebApp: {
+    title: APP_TITLE,
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Bitcoin Wars',
-    description:
-      'Gear up for an epic adventure and strategize your way to victory in Bitcoin Wars, an incredibly fun onchain game on Bitcoin.',
+    title: {
+      absolute: APP_TITLE,
+    },
+    description: APP_DESCRIPTION,
     images: '/maga/crypto-war-seo.png',
   },
 };
