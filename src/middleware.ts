c48a191797d61eebfcbd24c81929f5ff30a2980b
Peter <peter@newbitcoincity.com>
2024-08-01 11:49:51 +0700

    chore: remove middleware

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(
-    new URL(request.nextUrl.pathname.toLocaleLowerCase(), request.url),
-  );
-}
