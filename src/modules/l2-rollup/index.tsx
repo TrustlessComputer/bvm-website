c41eab6b7f361869a7568524d28060ceb8ffcba5
2712 <2712@trustless.computer>
2024-08-02 14:32:14 +0700

    feat: rollup l2

---
+++ src/modules/l2-rollup/index.tsx
@@ -0,0 +1,467 @@
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable jsx-a11y/alt-text */
+'use client';
+
+import ListTable, { ColumnProp } from '@/components/ListTable';
+import { MIN_DECIMAL } from '@/constants/constants';
+import CRollupL2API from '@/services/api/dapp/rollupl2';
+import { IRollupL2Info } from '@/services/api/dapp/rollupl2/interface';
+import { formatCurrency } from '@/utils/format';
+import {
+  Box,
+  Flex,
+  Text,
+  Tooltip,
+  Image,
+  Popover,
+  PopoverContent,
+  PopoverBody,
+  PopoverTrigger,
+} from '@chakra-ui/react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import s from './styles.module.scss';
+
+const L2Rollup = () => {
+  const [data, setData] = useState<IRollupL2Info[]>([]);
+
+  const hasIncrementedPageRef = useRef(false);
+  const rollupL2Api = new CRollupL2API();
+
+  const total = useMemo(() => {
+    const tps = data.reduce((accum, item) => accum + item.tps, 0);
+    const mgas = data.reduce((accum, item) => accum + item.mgas, 0);
+    const kbs = data.reduce((accum, item) => accum + item.kbs, 0);
+
+    return {
+      tps,
+      mgas,
+      kbs,
+    };
+  }, [data]);
+
+  useEffect(() => {
+    fetchData();
+    const interval = setInterval(() => {
+      fetchData();
+    }, 2000); // 2s
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await rollupL2Api.getRollupL2Info();
+      setData(res);
+    } catch (error) {
+    } finally {
+      hasIncrementedPageRef.current = false;
+    }
+  };
+
+  const labelConfig = {};
+
+  const columns: ColumnProp[] = useMemo(() => {
+    return [
+      {
+        id: 'network',
+        label: (
+          <Flex pl={'8px'} gap={'4px'}>
+            Network
+          </Flex>
+        ),
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '16px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Popover>
+              <PopoverTrigger>
+                <Flex
+                  alignItems={'center'}
+                  width={'100%'}
+                  justifyContent={'space-between'}
+                  paddingLeft={'16px'}
+                  cursor={'pointer'}
+                  _hover={{
+                    textDecoration: 'underline',
+                  }}
+                >
+                  <Text className={s.title}>{data.name}</Text>
+                </Flex>
+              </PopoverTrigger>
+              <PopoverContent
+                className={s.popoverContent}
+                bg="#fff"
+                w={'360px'}
+                _focus={{
+                  boxShadow: 'none !important',
+                }}
+              >
+                <PopoverBody
+                  className={s.popoverBody}
+                  backgroundColor="white !important"
+                  w={'100%'}
+                >
+                  <Text fontSize={'16px'} textAlign={'center'}>
+                    {data.name}
+                  </Text>
+                  <Flex direction={'column'} gap={'4px'} mt={'16px'}>
+                    <Flex direction={'row'} justifyContent={'space-between'}>
+                      <Text>Website</Text>
+                      <Text
+                        cursor={'pointer'}
+                        _hover={{
+                          textDecoration: 'underline',
+                        }}
+                        onClick={() =>
+                          data.website ? window.open(data.website) : {}
+                        }
+                        maxW="200px"
+                        overflow="hidden"
+                        textOverflow="ellipsis"
+                      >
+                        {data.website
+                          ? data.website.replaceAll('https://', '')
+                          : '-'}
+                      </Text>
+                    </Flex>
+                    <Flex direction={'row'} justifyContent={'space-between'}>
+                      <Text>Explorer</Text>
+                      <Text
+                        cursor={'pointer'}
+                        _hover={{
+                          textDecoration: 'underline',
+                        }}
+                        onClick={() =>
+                          data.explorer ? window.open(data.explorer) : {}
+                        }
+                        maxW="200px"
+                        overflow="hidden"
+                        textOverflow="ellipsis"
+                      >
+                        {data.explorer
+                          ? data.explorer.replaceAll('https://', '')
+                          : '-'}
+                      </Text>
+                    </Flex>
+                    <Flex direction={'row'} justifyContent={'space-between'}>
+                      <Flex direction={'row'} alignItems={'center'} gap={'4px'}>
+                        <Text>Stack</Text>
+                        <Tooltip label={'The tech stack used by the network.'}>
+                          <Image
+                            cursor={'pointer'}
+                            width="18px"
+                            height="18px"
+                            alt="tooltip"
+                            src={'/icons/ic-tooltip-blue.svg'}
+                          />
+                        </Tooltip>
+                      </Flex>
+                      <Text>{data.stack || '-'}</Text>
+                    </Flex>
+                    <Flex direction={'row'} justifyContent={'space-between'}>
+                      <Flex direction={'row'} alignItems={'center'} gap={'4px'}>
+                        <Text>DA</Text>
+                        <Tooltip
+                          label={
+                            'Where Data Availability is located. Important for the network security.'
+                          }
+                        >
+                          <Image
+                            cursor={'pointer'}
+                            width="18px"
+                            height="18px"
+                            alt="tooltip"
+                            src={'/icons/ic-tooltip-blue.svg'}
+                          />
+                        </Tooltip>
+                      </Flex>
+                      <Text>{data.da || '-'}</Text>
+                    </Flex>
+                    <Flex direction={'row'} justifyContent={'space-between'}>
+                      <Text>Provider</Text>
+                      <Text>{data.provider || '-'}</Text>
+                    </Flex>
+                  </Flex>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          );
+        },
+      },
+      {
+        id: 'block',
+        label: 'Block',
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '16px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex
+              alignItems={'center'}
+              width={'100%'}
+              justifyContent={'space-between'}
+              cursor="pointer"
+              onClick={() => {
+                window.open(data.explorer);
+              }}
+              _hover={{
+                textDecoration: 'underline',
+              }}
+            >
+              <Text className={s.title}>{data.block_number}</Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'tps',
+        label: (
+          <Flex
+            style={{
+              alignSelf: 'center',
+              width: '100%',
+              textTransform: 'uppercase',
+            }}
+          >
+            TPS
+          </Flex>
+        ),
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '14px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex gap={3} alignItems={'center'} width={'60px'}>
+              <Text className={s.title}>
+                {formatCurrency(data?.tps, MIN_DECIMAL, MIN_DECIMAL)}
+              </Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'mgas',
+        label: <Flex width={'60px'}>Mgas/s</Flex>,
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '14px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex gap={3} alignItems={'center'} width={'98px'}>
+              <Text className={s.title}>
+                {formatCurrency(data?.mgas, MIN_DECIMAL, MIN_DECIMAL)}
+              </Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'kbs',
+        label: (
+          <Flex
+            style={{
+              alignSelf: 'center',
+              width: '100%',
+              textTransform: 'uppercase',
+            }}
+          >
+            KB/s
+          </Flex>
+        ),
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '14px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex gap={3} alignItems={'center'} width={'60px'}>
+              <Text className={s.title}>
+                {formatCurrency(data?.kbs, MIN_DECIMAL, MIN_DECIMAL)}
+              </Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'stack',
+        label: (
+          <Flex
+            style={{
+              alignSelf: 'center',
+              width: '100%',
+              textTransform: 'uppercase',
+            }}
+          >
+            Stack
+          </Flex>
+        ),
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '14px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex gap={3} alignItems={'center'} width={'100%'}>
+              <Text className={s.title}>{data.stack || '-'}</Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'da',
+        label: (
+          <Flex
+            style={{
+              alignSelf: 'center',
+              width: '100%',
+              textTransform: 'uppercase',
+            }}
+          >
+            DA
+          </Flex>
+        ),
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '14px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex gap={3} alignItems={'center'} width={'100%'}>
+              <Text className={s.title}>{data.da || '-'}</Text>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'settlement',
+        label: (
+          <Flex
+            style={{
+              alignSelf: 'center',
+              width: '100%',
+              textTransform: 'uppercase',
+            }}
+          >
+            Settlement
+          </Flex>
+        ),
+        labelConfig,
+        config: {
+          borderBottom: 'none',
+          fontSize: '14px',
+          fontWeight: 500,
+          verticalAlign: 'middle',
+          letterSpacing: '-0.5px',
+        },
+        render(data: IRollupL2Info) {
+          return (
+            <Flex alignItems={'center'} width={'100%'}>
+              <Text className={s.title}>{data.settlement || '-'}</Text>
+            </Flex>
+          );
+        },
+      },
+    ];
+  }, []);
+
+  const renderItemTotal = (title: string, value: string, tooltip?: string) => {
+    return (
+      <Flex direction={'column'} alignItems={'center'}>
+        <Flex direction={'row'} alignItems={'center'} gap={'4px'}>
+          <Text className={s.total_title}>{title}</Text>
+          {tooltip && (
+            <Tooltip label={tooltip}>
+              <Image
+                cursor={'pointer'}
+                width="18px"
+                height="18px"
+                alt="tooltip"
+                src={'/icons/ic-tooltip-blue.svg'}
+              />
+            </Tooltip>
+          )}
+        </Flex>
+        <Text fontSize={'18px'} fontWeight={'500'}>
+          {value}
+        </Text>
+      </Flex>
+    );
+  };
+  return (
+    <Box className={s.container}>
+      <Flex direction={'column'} w="100%" maxW={'1024px'} gap={'16px'}>
+        <Flex
+          className={s.totalContainer}
+          bg="#FAFAFA"
+          w="100%"
+          direction={'column'}
+          gap={'8px'}
+        >
+          <Text fontSize={'24px'} fontWeight={'600'} textAlign={'center'}>
+            Totals
+          </Text>
+          <Flex w="100%" direction={'row'} justifyContent={'space-evenly'}>
+            {renderItemTotal(
+              'TPS',
+              formatCurrency(total.tps, MIN_DECIMAL, MIN_DECIMAL),
+              'The total transactions per second',
+            )}
+            {renderItemTotal(
+              'Mgas/s',
+              formatCurrency(total.mgas, MIN_DECIMAL, MIN_DECIMAL),
+              'The total megagas (Million Gas) per second',
+            )}
+            {renderItemTotal(
+              'KB/s',
+              formatCurrency(total.kbs, MIN_DECIMAL, MIN_DECIMAL),
+              'Total KB per second',
+            )}
+          </Flex>
+        </Flex>
+        <Box w="100%" bg="#FAFAFA" minH={'450px'}>
+          <ListTable
+            data={data}
+            columns={columns}
+            className={s.tableContainer}
+          />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default L2Rollup;
---
+++ src/services/api/dapp/rollupl2/interface.ts
@@ -0,0 +1,13 @@
+export interface IRollupL2Info {
+  name: string;
+  block_number: number;
+  tps: number;
+  mgas: number;
+  kbs: number;
+  stack: string;
+  da: string;
+  settlement: string;
+  website: string;
+  explorer: string;
+  provider: string;
+}
