8e4765fcd4e8bcac71b63f2e25499c410a3de0c5
Jackie <1121@generative.xyz>
2024-08-09 11:30:41 +0700

    to hook

--- src/modules/blockchains/Buy/component4/CustomNode/ChainNode.tsx
+++ src/modules/blockchains/Buy/component4/CustomNode/ChainNode.tsx
@@ -17,6 +17,7 @@ import { memo } from 'react';
 import { Field } from '@/modules/blockchains/Buy/signals/useDragSignal';
 import useFormDappToFormChain from '../../hooks/useFormDappToFormChain';
 import { chainKeyToDappKey, cloneDeep, dappKeyToChainKey } from '../../utils';
+import useGettingDappLego from '../../hooks/useGettingDappLego';
 
 export type DataNode = Node<
   {
@@ -36,90 +37,13 @@ export type DataNode = Node<
 >;
 
 function ChainNode({ data, isConnectable }: NodeProps<DataNode>) {
-  const { parsedCategories, categories } = useModelCategoriesStore();
-  const { draggedFields, setDraggedFields } = useDragStore();
+  useGettingDappLego();
 
-  const { field, setFields } = useOrderFormStoreV3();
+  const { parsedCategories } = useModelCategoriesStore();
+  const { draggedFields } = useDragStore();
+
+  const { field } = useOrderFormStoreV3();
   const { isCapture } = useCaptureStore();
-  const { dappCount } = useFormDappToFormChain();
-
-  const setDappLegoToChain = () => {
-    let newDraggedFields = cloneDeep(draggedFields);
-    const newField = cloneDeep(field);
-
-    for (const key in dappCount) {
-      const _key = dappKeyToChainKey(key);
-
-      for (const category of categories || []) {
-        if (category.isChain) continue;
-
-        for (const option of category.options) {
-          if (!dappCount[chainKeyToDappKey(_key)] || option.key !== _key)
-            continue;
-
-          if (!newDraggedFields.includes(category.key)) {
-            newDraggedFields.push(category.key);
-
-            if (category.multiChoice) {
-              newField[category.key].value = [
-                ...((newField[category.key].value || []) as any[]),
-                option.key,
-              ];
-            } else {
-              newField[category.key].value = option.key;
-              newField[category.key].dragged = true;
-            }
-          } else {
-            if (
-              category.multiChoice &&
-              !(field[category.key].value || ([] as any)).includes(option.key)
-            ) {
-              newField[category.key].value = [
-                ...((newField[category.key].value || []) as any[]),
-                option.key,
-              ];
-              newField[category.key].dragged = true;
-            }
-          }
-        }
-      }
-    }
-
-    for (const key in newField) {
-      const category = categories?.find((i) => i.key === key);
-
-      if (!newField[key].value || !category || category.isChain) continue;
-
-      if (Array.isArray(newField[key].value)) {
-        newField[key].value = ((newField[key].value || []) as string[]).filter(
-          (keyAsAValue) => {
-            return (
-              typeof dappCount[chainKeyToDappKey(keyAsAValue)] === 'number'
-            );
-          },
-        );
-
-        if (newField[key].value.length === 0) {
-          newField[key].value = null;
-          newField[key].dragged = false;
-          newDraggedFields = newDraggedFields.filter((i) => i !== category.key);
-        }
-      } else {
-        if (!dappCount[chainKeyToDappKey(newField[key].value as string)]) {
-          newField[key].value = null;
-          newField[key].dragged = false;
-          newDraggedFields = newDraggedFields.filter((i) => i !== category.key);
-        }
-      }
-    }
-
-    setFields(newField);
-    setDraggedFields(newDraggedFields);
-  };
-
-  React.useEffect(() => {
-    setDappLegoToChain();
-  }, [dappCount]);
 
   return (
     <div className={`${s.wrapperBox} ${cn(s[`borderColor_${data.status}`])}`}>
