981aa763577e4a080e9544ee66af57b4b916e2b5
2712 <2712@trustless.computer>
2024-08-16 16:04:26 +0700

    update heartbeat

--- src/modules/l2-rollup/index.tsx
+++ src/modules/l2-rollup/index.tsx
@@ -456,6 +456,7 @@ const L2Rollup = () => {
                     cursor={'pointer'}
                     className={s.title}
                     fontSize={'14px !important'}
+                    color={`${txColor} !important`}
                   >
                     LEVEL {data.level || '-'}
                   </Text>
