fd3c8f98b31e859fbea4c8625b7005daae51dd1d
Jackie <1121@generative.xyz>
2024-08-16 09:51:46 +0700

    update

--- src/modules/blockchains/Buy/hooks/useFetchingTemplate.ts
+++ src/modules/blockchains/Buy/hooks/useFetchingTemplate.ts
@@ -35,7 +35,6 @@ export default function useFetchingTemplate() {
   const params = useParams();
 
   const { order, isAAInstalled, isUpdateFlow } = useChainProvider();
-  const { order, isAAInstalled, isUpdateFlow } = useChainProvider();
   const { nodes, setNodes, edges, setEdges } = useFlowStore();
   const {
     setParsedCategories,
