fc15c38f37ed7b7d59e2988517be73c6f77cc13d
Jackie <1121@generative.xyz>
2024-08-19 17:16:55 +0700

    comment hard coded

--- src/modules/blockchains/Buy/hooks/useFetchingTemplate.ts
+++ src/modules/blockchains/Buy/hooks/useFetchingTemplate.ts
@@ -78,8 +78,8 @@ export default function useFetchingTemplate() {
     const newFields = cloneDeep(field);
     const categoryMapping: Record<string, IModelCategory> = {};
     const [categories, templates] = await Promise.all([
-      // getModelCategories(l2ServiceUserAddress),
-      getModelCategories('0x4113ed747047863Ea729f30C1164328D9Cc8CfcF'),
+      getModelCategories(l2ServiceUserAddress),
+      // getModelCategories('0x4113ed747047863Ea729f30C1164328D9Cc8CfcF'),
       getTemplates(),
     ]);
 
