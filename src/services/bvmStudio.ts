0382c45d76e96419ba43115945197fe4a61793b6
0xd22f9c <dr.cat911@bvm.network>
2024-08-22 15:59:03 +0700

    update auth

--- src/services/bvmStudio.ts
+++ src/services/bvmStudio.ts
@@ -1,6 +1,34 @@
 import { WP_API_URL } from '@/config';
+const AUTH_KEY = process.env.WP_AUTH_BVM_STUDIO_KEY;
 
 const BASE_URL = `${WP_API_URL}/bvm-studio/position/v1`;
+
+async function fetchWithAuth(url: string, options: RequestInit = {}): Promise<Response> {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${AUTH_KEY}`,
+    ...options.headers,
+  };
+
+  return fetch(url, { ...options, headers });
+}
+
+export async function addPositions( positions: BoxItemPosition[]): Promise<void> {
+  try {
+    const response = await fetchWithAuth(`${BASE_URL}/add-list`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(positions),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to add related posts');
+    }
+  } catch (error) {
+    console.error('Error adding related posts:', error);
+  }
+}
 
 // Fetch related posts by post ID
 export async function getPositionsByOrderId(orderId: string): Promise<BoxItemPosition[]> {
@@ -21,7 +49,7 @@ export async function getPositionsByOrderId(orderId: string): Promise<BoxItemPos
 
 export async function updatePositionsByOrderId(orderId: string, positions: BoxItemPosition[]): Promise<void> {
   try {
-    const response = await fetch(`${BASE_URL}/update-list/${orderId}`, {
+    const response = await fetchWithAuth(`${BASE_URL}/update-list/${orderId}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -38,7 +66,7 @@ export async function updatePositionsByOrderId(orderId: string, positions: BoxIt
 
 export async function deletePositionsByOrderId(orderId: string): Promise<void> {
   try {
-    const response = await fetch(`${BASE_URL}/delete-list/${orderId}`, {
+    const response = await fetchWithAuth(`${BASE_URL}/delete-list/${orderId}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -49,6 +77,23 @@ export async function deletePositionsByOrderId(orderId: string): Promise<void> {
     }
   } catch (error) {
     console.error('Error deleting related posts:', error);
+  }
+}
+
+export async function addPositionItem(position: BoxItemPosition): Promise<void> {
+  try {
+    const response = await fetchWithAuth(`${BASE_URL}/add-item`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(position),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to add related posts');
+    }
+  } catch (error) {
+    console.error('Error adding related posts:', error);
   }
 }
 
@@ -70,7 +115,7 @@ export async function getPositionItemById(id: number): Promise<BoxItemPosition |
 
 export async function updatePositionItemById(id: number, position: BoxItemPosition): Promise<void> {
   try {
-    const response = await fetch(`${BASE_URL}/update-item/${id}`, {
+    const response = await fetchWithAuth(`${BASE_URL}/update-item/${id}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -87,7 +132,7 @@ export async function updatePositionItemById(id: number, position: BoxItemPositi
 
 export async function  deletePositionItemById(id: number): Promise<void> {
   try {
-    const response = await fetch(`${BASE_URL}/delete-item/${id}`, {
+    const response = await fetchWithAuth(`${BASE_URL}/delete-item/${id}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
