80a3fbad20675c47b0c64691d0d5d469401ce83b
0xd22f9c <dr.cat911@bvm.network>
2024-08-22 15:37:29 +0700

    add service bvm studio position apis

--- src/services/bvmStudio.ts
+++ src/services/bvmStudio.ts
@@ -1,11 +1,11 @@
 import { WP_API_URL } from '@/config';
 
-const BASE_URL = `${WP_API_URL}/wp-json/blog/v1`;
+const BASE_URL = `${WP_API_URL}/bvm-studio/position/v1`;
 
 // Fetch related posts by post ID
-export async function fetchRelatedPostsById(postId: number): Promise<Blog[]> {
+export async function getPositionsByOrderId(orderId: string): Promise<BoxItemPosition[]> {
   try {
-    const response = await fetch(`${BASE_URL}/related/${postId}`, {
+    const response = await fetch(`${BASE_URL}/get-list/${orderId}`, {
       method: 'GET',
       cache: 'force-cache',
     });
@@ -19,61 +19,84 @@ export async function fetchRelatedPostsById(postId: number): Promise<Blog[]> {
   }
 }
 
-// Fetch single post by ID with view counter update
-export async function fetchPostById(slug: string): Promise<Blog | null> {
+export async function updatePositionsByOrderId(orderId: string, positions: BoxItemPosition[]): Promise<void> {
   try {
-    const response = await fetch(`${BASE_URL}/get/${slug}`, {
-      method: 'GET',
+    const response = await fetch(`${BASE_URL}/update-list/${orderId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(positions),
     });
     if (!response.ok) {
-      throw new Error('Failed to fetch post');
+      throw new Error('Failed to update related posts');
+    }
+  } catch (error) {
+    console.error('Error updating related posts:', error);
+  }
+}
+
+export async function deletePositionsByOrderId(orderId: string): Promise<void> {
+  try {
+    const response = await fetch(`${BASE_URL}/delete-list/${orderId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete related posts');
+    }
+  } catch (error) {
+    console.error('Error deleting related posts:', error);
+  }
+}
+
+export async function getPositionItemById(id: number): Promise<BoxItemPosition | null> {
+  try {
+    const response = await fetch(`${BASE_URL}/get-item/${id}`, {
+      method: 'GET',
+      cache: 'force-cache',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch related posts');
     }
     return await response.json();
   } catch (error) {
-    console.error('Error fetching post:', error);
+    console.error('Error fetching related posts:', error);
     return null;
   }
 }
 
-// Fetch all posts with pagination, sorting, and view count
-export async function fetchAllPosts(params: {
-  per_page?: number;
-  page?: number;
-  order?: string;
-  orderBy?: string,
-  tag?: string
-  author?: string
-}): Promise<Posts> {
-
-  const { per_page = 10, page = 1, order = 'desc', orderBy = 'date', tag, author } = params;
-
-  // Construct query parameters dynamically
-  const queryParams = new URLSearchParams({
-    per_page: per_page.toString(),
-    page: page.toString(),
-    order,
-    orderBy,
-  });
-
-  if (tag) {
-    queryParams.append('tag', tag);
-  }
-
-  if (author) {
-    queryParams.append('author', author);
-  }
-
-  const url = `${BASE_URL}/posts?${queryParams.toString()}`;
+export async function updatePositionItemById(id: number, position: BoxItemPosition): Promise<void> {
   try {
-
-    const response = await fetch(url, { method: 'GET', cache: 'no-cache' });
-    console.log('response', response);
+    const response = await fetch(`${BASE_URL}/update-item/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(position),
+    });
     if (!response.ok) {
-      throw new Error('Failed to fetch posts');
+      throw new Error('Failed to update related posts');
     }
-    return await response.json();
   } catch (error) {
-    console.error('Error fetching posts:', error);
-    return { data: [], total: 0, page, per_page };
+    console.error('Error updating related posts:', error);
+  }
+}
+
+export async function  deletePositionItemById(id: number): Promise<void> {
+  try {
+    const response = await fetch(`${BASE_URL}/delete-item/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete related posts');
+    }
+  } catch (error) {
+    console.error('Error deleting related posts:', error);
   }
 }
---
+++ src/types/bvm-studio.d.ts
@@ -0,0 +1,7 @@
+interface BoxItemPosition {
+  id: number;
+  order_id: string,
+  position_id: string,
+  x: number,
+  y: number
+}
