1de0fad31de7ff66efb399fe3f38bd3d281b6ef8
Archaon <archaon1574@bvm.network>
2024-08-22 17:33:33 +0700

    fix build

--- src/modules/landing/Componets/Article/ItemArticle/index.tsx
+++ src/modules/landing/Componets/Article/ItemArticle/index.tsx
@@ -10,7 +10,7 @@ export default function ItemArticle({ data }: { data: IBlog }) {
     const tmp = LOGOS.filter((itemLogo) => {
       return itemLogo.id === data.logo;
     });
-    return tmp.length ? tmp[0].img : '';
+    return tmp.length ? tmp[0].blackLogo : '';
   }, [data]);
   return (
     <div className={s.itemArticle}>
