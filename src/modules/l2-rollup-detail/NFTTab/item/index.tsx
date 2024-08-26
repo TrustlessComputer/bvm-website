7b1f2268e2717b7d1ed70d436786a11f6d16b413
0xmegalodon <stephen@newbitcoincity.com>
2024-08-26 16:28:58 +0700

    -: update default nft image

--- src/modules/l2-rollup-detail/NFTTab/item/index.tsx
+++ src/modules/l2-rollup-detail/NFTTab/item/index.tsx
@@ -6,7 +6,7 @@ import { IRollupNFTDetail } from '@/services/api/dapp/rollupl2-detail/interface'
 const NFTItem = ({item}: {item: IRollupNFTDetail}) => {
   return (
     <Flex direction={'column'} className={s.container}>
-      <Image borderTopRadius={'12px'} w={'100%'} aspectRatio={1} src={item.token_uri || '/images/default_nft.jpg'}/>
+      <Image borderTopRadius={'12px'} w={'100%'} aspectRatio={1} src={item.token_uri || '/images/default_nft.png'}/>
       <Flex direction={'column'} p={'8px'}>
         <Text>#{item.token_id}</Text>
         {/*<Text color={'#898989'}>{item.value}</Text>*/}
