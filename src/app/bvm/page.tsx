d38e795383ddf5e5ce6a296699660f46356db667
Archaon <archaon1574@bvm.network>
2024-08-27 11:46:02 +0700

    update metadata

--- src/app/bvm/page.tsx
+++ src/app/bvm/page.tsx
@@ -1,7 +1,33 @@
-'use client';
+// 'use client';
 
+import { CDN_URL } from '@/config';
+import { APP_NAME } from '@/config/metadata';
 import MainLayout from '@/layouts/MainLayout';
 import BVMModule from '@/modules/bvm_v4';
+import { Metadata } from 'next';
+
+const THUMBNAIL = `${CDN_URL}/metadata/bvm-metadata.png`;
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        url: THUMBNAIL,
+        width: 1200,
+        height: 630,
+        alt: APP_NAME,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: THUMBNAIL,
+        alt: APP_NAME,
+      },
+    ],
+  },
+};
 
 const BVMPage = () => {
   return (
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,5 +1,33 @@
 import MainLayout from '@layouts/MainLayout';
 import TeamModule from '@/modules/team';
+import { CDN_URL } from '@/config';
+import { Metadata } from 'next';
+import { APP_NAME } from '@/config/metadata';
+
+const THUMBNAIL = `${CDN_URL}/metadata/bvm-team-metadata.png`;
+
+export const metadata: Metadata = {
+  description:
+    'BVM is a crypto research and development team. Our mission is to upgrade Bitcoin beyond just a currency.',
+  openGraph: {
+    images: [
+      {
+        url: THUMBNAIL,
+        width: 1200,
+        height: 630,
+        alt: APP_NAME,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: THUMBNAIL,
+        alt: APP_NAME,
+      },
+    ],
+  },
+};
 
 const TeamPage = () => {
   return (
