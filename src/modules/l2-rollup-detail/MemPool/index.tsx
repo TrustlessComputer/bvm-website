03a7be27540fce955ee17a351af0890ff565a107
0xmegalodon <stephen@newbitcoincity.com>
2024-08-29 11:30:09 +0700

    -: update setup app

---
+++ src/modules/l2-rollup-detail/MemPool/index.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react"
+import s from './styles.module.scss';
+
+const MemPoolModule = () => {
+  return (
+    <Flex className={s.container}>
+      MemPoolModule
+    </Flex>
+  )
+};
+
+export default MemPoolModule;
