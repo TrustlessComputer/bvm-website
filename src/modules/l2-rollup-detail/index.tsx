8c381cf688eaf1754c13a2381872ab5cbd955909
2712 <2712@trustless.computer>
2024-09-05 13:51:13 +0700

    update heartbeat

--- src/modules/l2-rollup-detail/index.tsx
+++ src/modules/l2-rollup-detail/index.tsx
@@ -87,7 +87,7 @@ const L2RollupDetail = () => {
             onClick={() => router.push(HEART_BEAT)}
           >
             <Image w={'24px'} src={'/heartbeat/ic-back.svg'} />
-            <Text>Bitcoin Heartbeat Project</Text>
+            <Text>Bitcoin Heartbeats Project</Text>
           </Flex>
           <Flex alignItems={'center'} gap={'4px'} position={'relative'}>
             <SearchAddress
