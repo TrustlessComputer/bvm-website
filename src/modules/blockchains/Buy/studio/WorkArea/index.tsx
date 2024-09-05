f3899c5641355a945324c2990797d6f79698e554
0xd22f9c <dr.cat911@bvm.network>
2024-09-05 16:14:48 +0700

    fix build

--- src/modules/blockchains/Buy/studio/WorkArea/index.tsx
+++ src/modules/blockchains/Buy/studio/WorkArea/index.tsx
@@ -2,9 +2,8 @@ import ActionsWorkArea from '@/modules/blockchains/Buy/studio/ActionsWorkArea';
 import TopWorkArea from '@/modules/blockchains/Buy/studio/TopWorkArea';
 import RightContent from '@/modules/blockchains/Buy/studio/WorkArea/RightContent';
 import s from '@/modules/blockchains/Buy/styles_v6.module.scss';
-// import ButtonStartChat from '../ButtonStartChat';
-import LoadingOverlay from './LoadingOverlay';
 import ButtonStartChat from '../ButtonStartChat';
+import LoadingOverlay from './LoadingOverlay';
 
 export default function WorkArea() {
   return (
