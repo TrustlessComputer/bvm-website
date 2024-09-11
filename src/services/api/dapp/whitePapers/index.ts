715267a8628b92fb3e488a3136d53b0b77a8ce2e
0xmegalodon <stephen@newbitcoincity.com>
2024-09-11 11:37:13 +0700

    -: update code white paper

--- src/services/api/dapp/whitePapers/index.ts
+++ src/services/api/dapp/whitePapers/index.ts
@@ -2,6 +2,7 @@ import CDappApiClient from '@/services/api/dapp/dapp.client';
 import { setWhitePapers } from '@/stores/states/dapp/reducer';
 import { useAppDispatch } from '@/stores/hooks';
 import { IWhitePaper } from '@/services/api/dapp/whitePapers/interface';
+import axios from 'axios';
 
 class CWhitePaperAPI {
   private api = new CDappApiClient().api;
@@ -29,6 +30,22 @@ class CWhitePaperAPI {
     const data: any = await this.api.get(this.getUrl(`detail/${id}`));
     return data;
   };
+
+  testCreateWhitePaper = async (content: string): Promise<any> => {
+    return axios.post('https://fxz7dxfvi58xv2-8000.proxy.runpod.net/v1/chat/completions',
+      {
+        "model": "meta-llama/Meta-Llama-3.1-70B-Instruct",
+        "stream": false,
+        "messages":[
+          {"role": "user", "content": content}
+        ]},
+      {
+        headers: {
+          'Authorization': 'Bearer d50b6ba5169ea538a71fe7b0685b755823a3746934fa3cc4',
+          'Content-Type': 'application/json',
+        }
+      });
+  }
 }
 
 export default CWhitePaperAPI;
