360efeab587ca128757bad78ee1931b70a41b468
Max <5008@generative.xyz>
2024-09-16 14:09:36 +0700

    fix edge

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -264,6 +264,7 @@ function handleStatusEdgeByBox(status: any) {
   switch (status) {
     case 'draft':
     case 'setting_up':
+    case 'Drafting Modules':
     case EAirdropStatus.new:
     case '':
       //draft
@@ -291,7 +292,7 @@ function handleStatusEdgeByBox(status: any) {
 
 export default function handleStatusEdges(statusDapp: any, status: aa | string, idNode: string) {
   if (idNode === 'account_abstraction' || idNode === 'bridge_apps' || idNode === 'gaming_apps') {
-    console.log('status here', status);
+    console.log('status here', status, idNode);
     return handleStatusEdgeByBox(status);
   }
 
